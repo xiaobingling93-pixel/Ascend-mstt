1ea2a3e7ed9518054c9ae5dfe658efcfcdba3339
louyujing <louyujing@huawei.com>
2023-12-29 07:29:41 +0000

    update debug/accuracy_tools/api_accuracy_checker/test/ut/common/test_base_api.py.
    Signed-off-by: louyujing <louyujing@huawei.com>

--- debug/accuracy_tools/api_accuracy_checker/test/ut/common/test_base_api.py
+++ debug/accuracy_tools/api_accuracy_checker/test/ut/common/test_base_api.py
@@ -4,62 +4,25 @@ import os
 import shutil
 from api_accuracy_checker.common.base_api import BaseAPIInfo
 
+
 class TestBaseAPI(unittest.TestCase):
     def setUp(self):
         if os.path.exists('./forward'):
             shutil.rmtree('./forward')
         os.makedirs('./forward', mode=0o755)
-        self.api = BaseAPIInfo("test_api", True, True, "./", "forward", "backward")
+        self.api = BaseAPIInfo("test_api", "./", "forward")
 
     def test_analyze_element(self):
-        element = [1, 2, 3]
-        result = self.api.analyze_element(element)
-        self.assertEqual(result, [{'type': 'int', 'value': 1}, {'type': 'int', 'value': 2}, {'type': 'int', 'value': 3}])
+        element = [torch.tensor(1), torch.rand(2), 3]
+        self.api.analyze_element(element)
+        self.assertTrue(os.path.exists("./forward/test_api.0.pt"))
+        self.assertTrue(os.path.exists("./forward/test_api.1.pt"))
+        self.assertFalse(os.path.exists("./forward/test_api.2.pt"))
 
     def test_analyze_tensor(self):
         tensor = torch.tensor([1, 2, 3], dtype=torch.float32, requires_grad=True)
-        result = self.api.analyze_tensor(tensor)
-        self.assertEqual(result.get('type'), 'torch.Tensor')
-        self.assertTrue(result.get('requires_grad'))
-        self.assertTrue(os.path.exists(result.get('datapath')))
-
-    def test_analyze_builtin(self):
-        arg = slice(1, 10, 2)
-        result = self.api.analyze_builtin(arg)
-        self.assertEqual(result, {'type': 'slice', 'value': [1, 10, 2]})
-
-    def test_transfer_types(self):
-        data = 10
-        dtype = 'int'
-        result = self.api.transfer_types(data, dtype)
-        self.assertEqual(result, 10)
-
-    def test_is_builtin_class(self):
-        element = 10
-        result = self.api.is_builtin_class(element)
-        self.assertEqual(result, True)
-
-    def test_analyze_device_in_kwargs(self):
-        element = torch.device('cuda:0')
-        result = self.api.analyze_device_in_kwargs(element)
-        self.assertEqual(result, {'type': 'torch.device', 'value': 'cuda:0'})
-
-    def test_analyze_dtype_in_kwargs(self):
-        element = torch.float32
-        result = self.api.analyze_dtype_in_kwargs(element)
-        self.assertEqual(result, {'type': 'torch.dtype', 'value': 'torch.float32'})
-
-    def test_get_tensor_extremum(self):
-        data = torch.tensor([1, 2, 3])
-        result_max = self.api.get_tensor_extremum(data, 'max')
-        result_min = self.api.get_tensor_extremum(data, 'min')
-        self.assertEqual(result_max, 3)
-        self.assertEqual(result_min, 1)
-
-    def test_get_type_name(self):
-        name = "<class 'int'>"
-        result = self.api.get_type_name(name)
-        self.assertEqual(result, 'int')
+        self.api.analyze_tensor(tensor)
+        self.assertTrue(os.path.exists("./forward/test_api.0.pt"))
 
     def tearDown(self):
         if os.path.exists('./forward'):
