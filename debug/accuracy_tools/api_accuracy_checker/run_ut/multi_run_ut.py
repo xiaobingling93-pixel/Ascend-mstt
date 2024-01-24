856e9a64fb46afc2e8a55cdc5f4434c2fb019237
s30048155 <sunyiming14@huawei.com>
2024-01-24 10:11:17 +0800

    add real_data_path

--- debug/accuracy_tools/api_accuracy_checker/run_ut/multi_run_ut.py
+++ debug/accuracy_tools/api_accuracy_checker/run_ut/multi_run_ut.py
@@ -43,14 +43,14 @@ signal.signal(signal.SIGINT, signal_handler)
 signal.signal(signal.SIGTERM, signal_handler)
 
 
-ParallelUTConfig = namedtuple('ParallelUTConfig', ['forward_files', 'backward_files', 'out_path', 'num_splits', 'save_error_data_flag', 'jit_compile_flag', 'device_id', 'result_csv_path', 'total_items'])
+ParallelUTConfig = namedtuple('ParallelUTConfig', ['forward_files', 'backward_files', 'out_path', 'num_splits', 'save_error_data_flag', 'jit_compile_flag', 'device_id', 'result_csv_path', 'total_items','real_data_path'])
 
 
 def run_parallel_ut(config):
     processes = []
     device_id_cycle = cycle(config.device_id)
     if config.save_error_data_flag:
-        print_info_log(f"UT task error datas will be saved")
+        print_info_log("UT task error datas will be saved")
     print_info_log(f"Starting parallel UT with {config.num_splits} processes")
     progress_bar = tqdm(total=config.total_items, desc="Total items", unit="items")
 
@@ -63,7 +63,8 @@ def run_parallel_ut(config):
             '-d', str(dev_id),
             *(['-j'] if config.jit_compile_flag else []),
             *(['-save_error_data'] if config.save_error_data_flag else []),
-            '-csv_path', config.result_csv_path
+            '-csv_path', config.result_csv_path,
+            *(['-real_data_path'] if config.real_data_path else [])
         ]
         return cmd
 
@@ -96,7 +97,7 @@ def run_parallel_ut(config):
         for process in processes:
             process.communicate(timeout=None)
     except KeyboardInterrupt: 
-        print_warn_log("Interrupted by user, terminating processes and clear up...")
+        print_warn_log("Interrupted by user, terminating processes and cleaning up...")
     except Exception as e:
         print_error_log(f"An unexpected error occurred: {e}")
     finally:
@@ -132,7 +133,7 @@ def prepare_config(args):
         details_csv_path = get_validated_details_csv_path(result_csv_path)
         print_info_log(f"UT task result will be saved in {result_csv_path}")
         print_info_log(f"UT task details will be saved in {details_csv_path}")
-    return ParallelUTConfig(forward_splits, backward_splits, out_path, args.num_splits, args.save_error_data, args.jit_compile, args.device_id, result_csv_path, total_items)
+    return ParallelUTConfig(forward_splits, backward_splits, out_path, args.num_splits, args.save_error_data, args.jit_compile, args.device_id, result_csv_path, total_items, args.real_data_path)
 
 
 def main():
