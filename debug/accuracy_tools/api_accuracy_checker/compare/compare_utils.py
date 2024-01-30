033e218806a8bf43e59a6b05a90093a2ad55c29d
jiangchangting1 <jiangchangting@huawei.com>
2024-01-30 03:38:18 +0000

    新精度标准
    Signed-off-by: jiangchangting1 <jiangchangting@huawei.com>

--- debug/accuracy_tools/api_accuracy_checker/compare/compare_utils.py
+++ debug/accuracy_tools/api_accuracy_checker/compare/compare_utils.py
@@ -11,8 +11,9 @@ detail_test_rows = [[
             "双千指标",
             "双万指标",
             "错误率",
-            "均方根误差",
             "误差均衡性",
+            "均方根误差",
+            "小值域错误占比",
             "相对误差最大值",
             "相对误差平均值",
             "Status",
@@ -70,17 +71,16 @@ class CompareColumn:
         self.rel_err_thousandth = CompareConst.NA
         self.rel_err_ten_thousandth = CompareConst.NA
         self.error_rate = CompareConst.NA
-        self.builtin = CompareConst.NA
-        self.inf_or_nan = CompareConst.NA
-        self.RMSE = CompareConst.NA
         self.EB = CompareConst.NA
+        self.RMSE = CompareConst.NA
+        self.small_value_err_ratio = CompareConst.NA
         self.Max_rel_error = CompareConst.NA
         self.Mean_rel_error = CompareConst.NA
 
     def to_column_value(self, is_pass, message):
         return [self.bench_type, self.npu_type, self.shape, self.cosine_sim, self.max_abs_err, self.rel_err_hundredth,
-                self.rel_err_thousandth, self.rel_err_ten_thousandth, self.error_rate, self.RMSE, self.EB, 
-                self.Max_rel_error, self.Mean_rel_error, is_pass, message]
+                self.rel_err_thousandth, self.rel_err_ten_thousandth, self.error_rate, self.EB, self.RMSE, 
+                self.small_value_err_ratio, self.Max_rel_error, self.Mean_rel_error, is_pass, message]
 
 
 def check_dtype_comparable(x, y):
