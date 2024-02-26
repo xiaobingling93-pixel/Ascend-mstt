100fcc14ef5fcf6ad0f714a8ff30306929e93b15
lian <lianwaiyu@qq.com>
2024-02-26 08:34:15 +0000

    update sample/normal_sample/cube_only/main.cpp.
    Signed-off-by: lian <lianwaiyu@qq.com>

--- sample/normal_sample/cube_only/main.cpp
+++ sample/normal_sample/cube_only/main.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) Huawei Technologies Co., Ltd. 2022-2023. All rights reserved.
+ * Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
  */
 
 #include <cassert>
