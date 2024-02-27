5954c2002576d958920767b374feb7b682022435
s30048155 <sunyiming14@huawei.com>
2024-02-27 18:12:10 +0800

    update

--- debug/accuracy_tools/api_accuracy_checker/run_ut/run_ut.py
+++ debug/accuracy_tools/api_accuracy_checker/run_ut/run_ut.py
@@ -337,13 +337,13 @@ def _run_ut_parser(parser):
 def preprocess_forward_content(forward_content):
     processed_content = {}
     for key, value in forward_content.items():
-        base_key = key.rsplit('*', 1)[0]
+        base_key = key.rsplit('*', 1)[0] + "*0"
         if base_key in processed_content:
             existing_args = processed_content[base_key]['args']
             new_args = value['args']
             filtered_existing_args = [{k: v for k, v in arg.items() if k not in ['Max', 'Min']} for arg in existing_args if isinstance(arg, dict)]
             filtered_new_args = [{k: v for k, v in arg.items() if k not in ['Max', 'Min']} for arg in new_args if isinstance(arg, dict)]
-            if filtered_existing_args == filtered_new_args and processed_content[base_key]['kwargs'] == value['kwargs']:
+            if filtered_existing_args == filtered_new_args:
                 continue
             else:
                 processed_content[key] = value
