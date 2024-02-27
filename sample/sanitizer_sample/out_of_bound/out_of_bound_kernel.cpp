885bd56d7e6728c033c923242a61c5b12cdb61e5
zhengweifeng <zhengweifeng6@huawei.com>
2024-02-27 09:35:37 +0000

    update sample/sanitizer_sample/out_of_bound/out_of_bound_kernel.cpp.
    Signed-off-by: zhengweifeng <zhengweifeng6@huawei.com>

--- sample/sanitizer_sample/out_of_bound/out_of_bound_kernel.cpp
+++ sample/sanitizer_sample/out_of_bound/out_of_bound_kernel.cpp
@@ -19,7 +19,7 @@ extern "C" __global__ __aicore__ void out_of_bound_kernel(__gm__ uint8_t *gm)
         if (i == GetBlockIdx())
         {
             // 这里第22行CORE_OFFSET < NUM_DATA, 第23行多核写入GM时，写入的size大于偏移，导致出现内存踩踏
-            // 解决方法时将CORE_OFFSET替换成NUM_DATA
+            // 解决方法是将CORE_OFFSET替换成NUM_DATA
             xGm.SetGlobalBuffer((__gm__ half *)gm + i * CORE_OFFSET, NUM_DATA);
             DataCopy(xGm, xLm, NUM_DATA);
         }
