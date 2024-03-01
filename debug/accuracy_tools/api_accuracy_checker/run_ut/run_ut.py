da7147da398f8afcac06426fa1b709396624ebba
sunyiming <sunyiming14@huawei.com>
2024-03-01 02:24:06 +0000

    update debug/accuracy_tools/api_accuracy_checker/run_ut/run_ut.py.
    Signed-off-by: sunyiming <sunyiming14@huawei.com>

--- debug/accuracy_tools/api_accuracy_checker/run_ut/run_ut.py
+++ debug/accuracy_tools/api_accuracy_checker/run_ut/run_ut.py
@@ -339,31 +339,29 @@ def _run_ut_parser(parser):
 def preprocess_forward_content(forward_content):
     processed_content = {}
     base_keys_variants = {}
-
     for key, value in forward_content.items():
         base_key = key.rsplit('*', 1)[0]
         new_args = value['args']
         new_kwargs = value['kwargs']
         filtered_new_args = [{k: v for k, v in arg.items() if k not in ['Max', 'Min']} for arg in new_args if isinstance(arg, dict)]
-
         if base_key in base_keys_variants:
             is_duplicate = False
-            for variant in base_keys_variants[base_key]:
-                existing_args = processed_content[variant]['args']
-                existing_kwargs = processed_content[variant]['kwargs']
-                filtered_existing_args = [{k: v for k, v in arg.items() if k not in ['Max', 'Min']} for arg in existing_args if isinstance(arg, dict)]
-
+            for variant in base_keys_variants.get(base_key, []):
+                try:
+                    existing_args = processed_content[variant].get('args', [])
+                    existing_kwargs = processed_content[variant].get('kwargs', {})
+                    filtered_existing_args = [{k: v for k, v in arg.items() if k not in ['Max', 'Min']} for arg in existing_args if isinstance(arg, dict)]
+                except KeyError as e:
+                    print_error_log(f"KeyError: {e} when processing {key}")
                 if filtered_existing_args == filtered_new_args and existing_kwargs == new_kwargs:
                     is_duplicate = True
                     break
-
             if not is_duplicate:
                 processed_content[key] = value
                 base_keys_variants[base_key].append(key)
         else:
             processed_content[key] = value
             base_keys_variants[base_key] = [key]
-
     return processed_content
 
 
