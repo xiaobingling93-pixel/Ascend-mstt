4e961a2e85a8ebb412ce49002822c4ffa841f8db
caishangqiu <caishangqiu@huawei.com>
2024-03-06 18:07:19 +0800

    fix typo 2

--- debug/accuracy_tools/atat/atat.py
+++ debug/accuracy_tools/atat/atat.py
@@ -19,7 +19,7 @@ from api_accuracy_checker.run_ut.run_ut import _run_ut_parser, run_ut_command
 from ptdbg_ascend.src.python.ptdbg_ascend.parse_tool.cli import parse as cli_parse
 from api_accuracy_checker.run_ut.multi_run_ut import prepare_config, run_parallel_ut
 from api_accuracy_checker.compare.benchmark_compare import _benchmark_compare_parser, _benchmark_compare_command
-from api_accuracy_checker.run_ut_run_overflow_check import _run_overflow_check_parser, _run_overflow_check_command
+from api_accuracy_checker.run_ut.run_overflow_check import _run_overflow_check_parser, _run_overflow_check_command
 
 
 def main():
