8ac2c62d9da20f05f8f639d82e66bb215a401ac4
s30048155 <sunyiming14@huawei.com>
2024-03-29 10:25:50 +0800

    clean code

--- debug/accuracy_tools/ptdbg_ascend/src/python/ptdbg_ascend/hook_module/wrap_torch.py
+++ debug/accuracy_tools/ptdbg_ascend/src/python/ptdbg_ascend/hook_module/wrap_torch.py
@@ -33,16 +33,16 @@ with FileOpen(yaml_path, 'r') as f:
 def get_torch_ops():
     global WrapTorchOps
     _torch_ops = []
-    for op in WrapTorchOps:
-        parts = op.split('.')
-        if len(parts) > 1:
-            sub_module_name, sub_op = parts
-            sub_module = getattr(torch, sub_module_name, None)
-            if sub_module and sub_op in dir(sub_module):
-                _torch_ops.append(op)
+    for operation in WrapTorchOps:
+        operation_parts = operation.split('.')
+        if len(operation_parts) > 1:
+            operation_sub_module_name, operation_sub_op = operation_parts
+            operation_sub_module = getattr(torch, operation_sub_module_name, None)
+            if operation_sub_module and operation_sub_op in dir(operation_sub_module):
+                _torch_ops.append(operation)
         else:
-            if op in dir(torch):
-                _torch_ops.append(op)
+            if operation in dir(torch):
+                _torch_ops.append(operation)
     return set(_torch_ops)
 
 
