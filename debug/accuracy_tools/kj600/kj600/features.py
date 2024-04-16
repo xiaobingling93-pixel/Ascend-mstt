d9eb61debef76d94d50c0144632ade477e796cc2
pengxiaopeng <pengxiaopeng1@huawei.com>
2024-04-16 16:46:10 +0800

    training monitor

---
+++ debug/accuracy_tools/kj600/kj600/features.py
@@ -0,0 +1,45 @@
+import torch
+from torch.autograd.functional import jacobian
+
+
+@torch.no_grad
+def square_sum(x: torch.tensor):
+    return (x * x).sum()
+
+
+@torch.no_grad
+def eff_rank(param: torch.tensor, threshold=1e-10):
+    U, S, Vh = torch.linalg.svd(param.float())
+    rank = torch.sum(S > threshold)
+    return rank
+
+
+# modular neural tangent kernel
+@torch.no_grad
+def mNTK(module: torch.nn.Module, x: torch.tensor):
+    J_theta_l = jacobian(module, x)
+    mntk = torch.matmul(J_theta_l, J_theta_l.t())
+    return mntk
+
+
+@torch.no_grad
+def power_iteration(A, num_iterations):
+    b = torch.randn(A.size(1), 1)
+    for _ in range(num_iterations):
+        b = torch.matmul(A, b)
+        b_norm = torch.norm(b)
+        b = b / b_norm if b_norm != 0 else 0
+    eigval = torch.matmul(torch.matmul(b.t(), A), b)
+    return eigval
+
+
+@torch.no_grad
+def lambda_max_subsample(module: torch.nn.Module, x: torch.tensor, num_iterations=100, subsample_size=None):
+    mntk = mNTK(module, x)
+    if subsample_size is None:
+        subsample_size = min(mntk.size(0), mntk.size(1))
+    idx = torch.randperm(mntk.size(0))[:subsample_size]
+    subsampled = mntk[idx, :]
+    subsampled = subsampled[:, idx]
+    eigval = power_iteration(subsampled, num_iterations)
+    return eigval
---
+++ debug/accuracy_tools/kj600/kj600/optimizer_collect.py
@@ -0,0 +1,46 @@
+from collections import defaultdict
+import torch.distributed as dist
+
+
+def print_rank_0(message, debug=False, force=False):
+    if dist.is_initialized():
+        if dist.get_rank() == 0:
+            print(message)
+    else:
+        print(message)
+
+
+class MixPrecsionOptimizerMon:
+    wrapped_optimizer = None
+
+    @staticmethod
+    def set_wrapped_optimizer(_wrapped_optimizer):
+        MixPrecsionOptimizerMon.wrapped_optimizer = _wrapped_optimizer
+
+    def __init__(self) -> None:
+        self.fp16_to_fp32_param = {}
+
+    def fetch_mv(self, torch_opt, params2name):
+        mix_prec_opt = MixPrecsionOptimizerMon.wrapped_optimizer
+
+        if not self.fp16_to_fp32_param and mix_prec_opt is not None:
+            for fp16_group, fp32_group in zip(mix_prec_opt.float16_groups, mix_prec_opt.fp32_from_float16_groups):
+                for fp16_param, fp32_param in zip(fp16_group, fp32_group):
+                    self.fp16_to_fp32_param[fp16_param] = fp32_param
+
+        exp_avg_norm_dict = defaultdict(float)
+        exp_avg_sq_norm_dict = defaultdict(float)
+
+        for param, name in params2name.items():
+            if param in self.fp16_to_fp32_param:
+                param = self.fp16_to_fp32_param[param]
+            
+            if param in torch_opt.state:
+                exp_avg = torch_opt.state[param]["exp_avg"]
+                exp_avg_sq = torch_opt.state[param]["exp_avg_sq"]
+                exp_avg_norm = exp_avg.detach().norm()
+                exp_avg_sq_norm = exp_avg_sq.detach().norm()
+                exp_avg_norm_dict[name] = exp_avg_norm
+                exp_avg_sq_norm_dict[name] = exp_avg_sq_norm
+
+        return exp_avg_norm_dict, exp_avg_sq_norm_dict
