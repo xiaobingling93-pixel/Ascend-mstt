80ca4df70c0bc35d183f5e7e2484966a8aeca972
pengxiaopeng <958876660@qq.com>
2024-06-19 16:16:51 +0800

    梯度工具测试用例更新

--- debug/accuracy_tools/grad_tool/test/ut/test_grad_csv.py
+++ debug/accuracy_tools/grad_tool/test/ut/test_grad_csv.py
@@ -15,16 +15,12 @@ class TestGradCSV(unittest.TestCase):
                          GradStatCsv.generate_csv_header(level=LevelAdapter.level_adapter("L0"), bounds=[-1, 0, 1]))
 
     def test_level_L1_header(self):
-        self.assertEqual(['param_name', 'MD5', '(-inf, -1]', '(-1, 0]', '(0, 1]', '(1, inf)', '=0', 'max', 'min', 'norm', 'shape'], 
+        self.assertEqual(['param_name', 'max', 'min', 'norm', 'shape'], 
                          GradStatCsv.generate_csv_header(level=LevelAdapter.level_adapter("L1"), bounds=[-1, 0, 1]))
 
     def test_level_L2_header(self):
-        self.assertEqual(['param_name', 'MD5', 'max', 'min', 'norm', 'shape'], 
+        self.assertEqual(['param_name', '(-inf, -1]', '(-1, 0]', '(0, 1]', '(1, inf)', '=0', 'max', 'min', 'norm', 'shape'], 
                          GradStatCsv.generate_csv_header(level=LevelAdapter.level_adapter("L2"), bounds=[-1, 0, 1]))
-
-    def test_level_L3_header(self):
-        self.assertEqual(['param_name', 'MD5', '(-inf, -1]', '(-1, 0]', '(0, 1]', '(1, inf)', '=0', 'max', 'min', 'norm', 'shape'], 
-                         GradStatCsv.generate_csv_header(level=LevelAdapter.level_adapter("L3"), bounds=[-1, 0, 1]))
 
     def test_level_L0_content(self):
         generated_csv_line = GradStatCsv.generate_csv_line(
@@ -41,7 +37,7 @@ class TestGradCSV(unittest.TestCase):
                 param_name="model.conv2d", 
                 grad=grad_tensor,
                 bounds=[-1, 0, 1])
-        self.assertEqual(['model.conv2d', '678a6c7d9d9716682b56fda097d0936c', 0.25, 0.0, 0.5, 0.25, 0.0, 2.0, -2.0, 2.851315498352051, [2, 2]],
+        self.assertEqual(['model.conv2d', 2.0, -2.0, 2.851315498352051, [2, 2]],
                          generated_csv_line)
 
     def test_level_L2_content(self):
@@ -50,14 +46,5 @@ class TestGradCSV(unittest.TestCase):
                 param_name="model.conv2d", 
                 grad=grad_tensor,
                 bounds=[-1, 0, 1])
-        self.assertEqual(['model.conv2d', '678a6c7d9d9716682b56fda097d0936c', 2.0, -2.0, 2.851315498352051, [2, 2]],
-                         generated_csv_line)
-
-    def test_level_L3_content(self):
-        generated_csv_line = GradStatCsv.generate_csv_line(
-                level=LevelAdapter.level_adapter("L3"), 
-                param_name="model.conv2d", 
-                grad=grad_tensor,
-                bounds=[-1, 0, 1])
-        self.assertEqual(['model.conv2d', '678a6c7d9d9716682b56fda097d0936c', 0.25, 0.0, 0.5, 0.25, 0.0, 2.0, -2.0, 2.851315498352051, [2, 2]],
+        self.assertEqual(['model.conv2d', 0.25, 0.0, 0.5, 0.25, 0.0, 2.0, -2.0, 2.851315498352051, [2, 2]],
                          generated_csv_line)
--- debug/accuracy_tools/grad_tool/test/ut/test_grad_monitor.py
+++ debug/accuracy_tools/grad_tool/test/ut/test_grad_monitor.py
@@ -52,32 +52,13 @@ def test_grad_monitor():
         optimizer.step()
     return gm
 
-
-def test_grad_monitor_1():
-    gm = GradientMonitor(os.path.join(base_dir, "resources/test_save_grad.yaml"))
-    loss_fun = nn.CrossEntropyLoss()
-    test_module = TestModule()
-    nn.init.constant_(test_module.linear.weight, 1.0)
-    nn.init.constant_(test_module.linear.bias, 1.0)
-    gm.monitor(test_module)
-    optimizer = torch.optim.SGD(test_module.parameters(), lr=1e-2)
-    for input_data, label in zip(inputs, labels):
-        output = test_module(input_data)
-        loss = loss_fun(output, label)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-    return gm
-
-
 class TestGradMonitor(unittest.TestCase):
     def test_compare(self):
-        gm1 = test_grad_monitor()
-        gm2 = test_grad_monitor_1()
-        compare_output_path = os.path.join(os.path.dirname(gm1._output_path), "grad_compare")
-        GradComparator.compare_distributed(gm1._output_path, gm2._output_path, compare_output_path)
+        gm = test_grad_monitor()
+        compare_output_path = os.path.join(os.path.dirname(gm.grad_monitor._output_path), "grad_compare")
+        GradComparator.compare_distributed(gm.grad_monitor._output_path, gm.grad_monitor._output_path, compare_output_path)
         items = os.listdir(compare_output_path)
         self.assertEqual(len(items), 1)
         with open(os.path.join(compare_output_path, items[0], "similarities.csv"), 'r') as f:
             data = f.read()
-        self.assertEqual(hashlib.md5(data.encode("utf-8")).hexdigest(), "20441d98b8c8d14ee6f896ea29d01b14")
+        self.assertEqual(hashlib.md5(data.encode("utf-8")).hexdigest(), "3762fafc89c805e7863f50aaffaf8161")
