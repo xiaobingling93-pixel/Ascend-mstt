8f6ae03e18444805e67936ce8dd83598e9fe26c2
wangqingcai <wqc01202410@gmail.com>
2024-06-25 20:42:42 +0800

    add enuma value

--- debug/accuracy_tools/atat/pytorch/common/utils.py
+++ debug/accuracy_tools/atat/pytorch/common/utils.py
@@ -185,3 +185,10 @@ class Const:
 
     INPLACE_LIST = ["broadcast", "all_reduce", "reduce", "all_gather", "gather", "scatter", "reduce_scatter",
                     "_reduce_scatter_base", "_all_gather_base", "all_to_all_single"]
+
+    TASK_LIST = ["tensor", "statistics", "overflow_check", "free_benchmark"]
+    LEVEL_LIST = ["L0", "L1", "L2", "mix"]
+    STATISTICS = "statistics"
+    TENSOR = "tensor"
+    OVERFLOW_CHECK = "overflow_check"
+    FREE_BENCHMARK = "free_benchmark"
