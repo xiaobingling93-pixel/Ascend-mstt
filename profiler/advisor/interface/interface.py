9271f1abc98981b23a313294297eb5d398837eb3
kongdeshuo <1670690897@qq.com>
2024-07-05 16:26:45 +0800

    修改代码质料链接

--- profiler/advisor/interface/interface.py
+++ profiler/advisor/interface/interface.py
@@ -17,15 +17,15 @@ from profiler.advisor.analyzer.schedule.dispatch.timeline_op_dispatch_analyzer i
 class Interface:
     supported_analyzer = {
         "schedule": OrderedDict({
-            SupportedScopes.TIMELINE_FUSION_OPS: TimelineFusionOpsAnalyzer
+            SupportedScopes.TIMELINE_FUSION_OPS: TimelineFusionOpsAnalyzer,
+            SupportedScopes.TIMELINE_OP_DISPATCH: OpDispatchAnalyzer
         }),
         "computation": OrderedDict({
             SupportedScopes.DYNAMIC_SHAPE_ANALYSIS: DynamicShapeAnalyzer,
             SupportedScopes.AICPU_ANALYSIS: AicpuAnalyzer,
             SupportedScopes.OPERATOR_NO_BOUND_ANALYSIS: OperatorBoundAnalyzer,
             SupportedScopes.BLOCK_DIM_ANALYSIS: BlockDimAnalyzer,
-            SupportedScopes.GRAPH: FusionOPAnalyzer,
-            SupportedScopes.TIMELINE_OP_DISPATCH: OpDispatchAnalyzer
+            SupportedScopes.GRAPH: FusionOPAnalyzer
         }),
         "communication": OrderedDict(),
         "overall": OrderedDict({SupportedScopes.OVER_ALL: OverallSummaryAnalyzer}),
