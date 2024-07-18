ede591c3a2e96b9d6e705a80833d39c4dc991586
gitee <jiangchangting@huawei.com>
2024-07-18 17:28:15 +0800

    fix

--- debug/accuracy_tools/atat/pytorch/api_accuracy_checker/compare/algorithm.py
+++ debug/accuracy_tools/atat/pytorch/api_accuracy_checker/compare/algorithm.py
@@ -197,8 +197,8 @@ def check_norm_value(normal_value_mask, rel_err, rtol):
 
 def get_ulp_err(bench_output, device_output, dtype):
     parameters = ULP_PARAMETERS.get(dtype)
-    min_eb = (parameters.get('min_eb'), DEFAULT_THRESHOLD)[0]
-    exponent_num = (parameters.get('exponent_num'), DEFAULT_THRESHOLD)[0]
+    min_eb = (parameters.get('min_eb', DEFAULT_THRESHOLD))[0]
+    exponent_num = (parameters.get('exponent_num', DEFAULT_THRESHOLD))[0]
     abs_bench = np.abs(bench_output)
     eb = np.where(abs_bench == 0, 0, np.floor(np.log2(abs_bench)))
     eb = np.maximum(eb, min_eb)
