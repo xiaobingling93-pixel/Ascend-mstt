698018c5faa754ca26a700531893c6f05b539212
fanhong <2532845962@qq.com>
2024-07-23 22:46:00 +0800

    适配disaggregate_perf新接口，新增over_summary_analysis

--- profiler/advisor/analyzer/overall/overall_summary_analyzer.py
+++ profiler/advisor/analyzer/overall/overall_summary_analyzer.py
@@ -12,11 +12,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import copy
 import os
 
 from profiler.advisor.analyzer.base_analyzer import BaseAnalyzer
-from profiler.advisor.common import constant as const
 from profiler.advisor.display.html.render import HTMLRender
 from profiler.advisor.result.item import OptimizeItem, OptimizeRecord
 from profiler.advisor.result.result import OptimizeResult
@@ -26,21 +24,21 @@ from profiler.compare_tools.compare_interface.comparison_interface import Compar
 
 class OverallSummaryAnalyzer(BaseAnalyzer):
     OVERALL_SUMMARY_ANALYZER = "overall_summary_analysis"
-    advice_map = {
-        "Computing Time": "if you want more detailed advice please go to att_advisor_*.html",
-        "Uncovered Communication Time": "if you want more detailed advice please go to att_advisor_*.html",
-        "Free Time": "if you want more detailed advice please go to att_advisor_*.html"
-    }
-    time_name_map = {
-        "Computing Time": "computing",
-        "Uncovered Communication Time": "communication",
-        "Free Time": "free",
-        'Cube Time(Num)': 'Cube Time',
-        'Vector Time(Num)': 'Vector Time',
-        'Flash Attention Time(Forward)(Num)': 'Flash Attention Time(Forward)',
-        'Flash Attention Time(Backward)(Num)': 'Flash Attention Time(Backward)',
-        'Other Time': "Other Computing Time",
-        'SDMA Time(Num)': 'SDMA Time'
+    performance_time_dict = {
+        "Computing Time": "computing_time_ms",
+        "    -- Flash Attention": "fa_time_ms",
+        "    -- Conv": "conv_time_ms",
+        "    -- Matmul": "matmul_time_ms",
+        "    -- Vector": "vector_time_ms",
+        "    -- SDMA(Tensor Move)": "tensor_move_time_ms",
+        "    -- Other Cube": "other_cube_time_ms",
+        "Uncovered Communication Time": "uncovered_communication_time_ms",
+        "    -- Wait": "wait_time_ms",
+        "    -- Transmit": "transmit_time_ms",
+        "Free Time": "free_time_ms",
+        "    -- SDMA": "sdma_time_ms",
+        "    -- Free": "free_ms",
+        "E2E Time": "e2e_time_ms"
     }
 
     def __init__(self, collection_path: str, n_processes: int = 1, **kwargs):
@@ -50,18 +48,14 @@ class OverallSummaryAnalyzer(BaseAnalyzer):
         self._has_benchmark_profiling = False
         self._is_minimal_profiling = False
         self.cur_data = {}
-        self.cur_data_table = {}
         self.cur_bottleneck = {}
         self._disaggregate_perf = {}
         self._disaggregate_benchmark_perf = {}
         self.cur_advices = ""
-        self._headers = []
-        self._base_data = []
-        self._comparison_data = []
         self.html_render = HTMLRender()
         self.result = OptimizeResult()
         self.bottleneck_str = ""
-        self.bottleneck_table = {}
+        self.over_summary_analysis = {}
 
     @staticmethod
     def calculate_ratio(dividend, divisor):
@@ -88,13 +82,13 @@ class OverallSummaryAnalyzer(BaseAnalyzer):
 
     def process(self):
         self._disaggregate_perf = ComparisonInterface(self.collection_path).disaggregate_perf(Constant.OVERALL_COMPARE)
-        if self._has_benchmark_profiling:
-            self._disaggregate_benchmark_perf = (ComparisonInterface(self.benchmark_profiling_path)
-                                                 .disaggregate_perf(Constant.OVERALL_COMPARE))
         if not self._disaggregate_perf:
             return
         self._is_minimal_profiling = self._disaggregate_perf.get("minimal_profiling", False)
         self.cur_data["overall_data"] = self.get_time_category_dict(self._disaggregate_perf.get('overall', {}))
+        if self._has_benchmark_profiling:
+            self._disaggregate_benchmark_perf = ComparisonInterface(
+                self.benchmark_profiling_path).disaggregate_perf(Constant.OVERALL_COMPARE)
 
     def identify_bottleneck(self):
         overall_data = self.cur_data.get("overall_data")
@@ -112,8 +106,9 @@ class OverallSummaryAnalyzer(BaseAnalyzer):
             if not self._has_benchmark_profiling:
                 continue
             # add comparison bottleneck
-            base_duration = self.get_time_category_dict(self._disaggregate_benchmark_perf.get('overall', {})).get(
-                time_type)
+            base_duration = self.get_time_category_dict(
+                self._disaggregate_benchmark_perf.get('overall', {})
+            ).get(time_type)
             if time_value > base_duration:
                 ratio = "{:.2%}".format(self.calculate_ratio(time_value - base_duration, base_duration))
                 comparison_bottleneck += f"{time_type} exceeds the benchmark by {ratio}\n"
@@ -126,40 +121,63 @@ class OverallSummaryAnalyzer(BaseAnalyzer):
             self.process()
         self.identify_bottleneck()
         self.format_bottleneck()
-        self.format_cur_data()
+        self.format_over_summary_analysis()
         self.make_record()
         self.make_render()
         return self.result
 
     def format_bottleneck(self):
         result = ''
-        headers = []
-        data_list = []
-        data = []
-        for key, value in self.cur_bottleneck.items():
+        for _, value in self.cur_bottleneck.items():
             if not value:
                 continue
             result += f'{value} \n'
-            headers.append(key)
-            data.append(value)
-        data_list.append(data)
         self.bottleneck_str = result
-        self.bottleneck_table["headers"] = headers
-        self.bottleneck_table["data"] = data_list
 
-    def format_cur_data(self):
-        if not self.cur_data:
-            return
-        for data_type, data in self.cur_data.items():
-            if not data:
-                continue
-            if data_type not in list(self.time_name_map.values()):
-                data_list = list(data.values())
-            else:
-                data_list = [','.join(map(str, value)) for value in data.values()]
-            headers = list(data.keys())
-            data_table = {"headers": headers, "data": [data_list]}
-            self.cur_data_table[data_type] = copy.deepcopy(data_table)
+    def format_over_summary_analysis(self):
+        headers = ['Performance Index', 'Duration(ms)', 'Duration Ratio']
+        performance_data = self.get_analysis_data(self._disaggregate_perf)
+        benchmark_data = self.get_analysis_data(self._disaggregate_benchmark_perf)
+        if self._has_benchmark_profiling:
+            headers.append('Diff Duration(ms)')
+            self.format_analysis_with_benchmark(performance_data, benchmark_data, headers)
+        else:
+            self.format_analysis_only(performance_data, headers)
+
+    def get_analysis_data(self, data_dict: dict):
+        if not data_dict:
+            return {}
+        return {
+            **data_dict.get("overall"),
+            **data_dict.get("computing_time_disaggregate"),
+            **data_dict.get("communication_time_disaggregate"),
+            **data_dict.get("free_time_disaggregate"),
+        }
+
+    def format_analysis_only(self, performance_data: dict, headers: list):
+        res = []
+        total_duration = performance_data.get('e2e_time_ms', 0.0)
+        for time_name, time_key in self.performance_time_dict.items():
+            row = [time_name]
+            duration = performance_data.get(time_key, 0.0)
+            row.append("{:.3f}".format(duration))
+            row.append("{:.2%}".format(self.calculate_ratio(duration, total_duration)))
+            res.append(row)
+        self.over_summary_analysis["headers"] = headers
+        self.over_summary_analysis["data"] = res
+
+    def format_analysis_with_benchmark(self, performance_data: dict, benchmark_data: dict, headers: list):
+        res = []
+        total_duration = performance_data.get('e2e_time_ms', 0.0)
+        for time_name, time_key in self.performance_time_dict.items():
+            row = [time_name]
+            duration = performance_data.get(time_key, 0.0)
+            row.append("{:.3f}".format(duration))
+            row.append("{:.2%}".format(self.calculate_ratio(duration, total_duration)))
+            row.append("{:.3f}".format(duration - benchmark_data.get(time_key, 0.0)))
+            res.append(row)
+        self.over_summary_analysis["headers"] = headers
+        self.over_summary_analysis["data"] = res
 
     def make_record(self):
         """
@@ -174,10 +192,12 @@ class OverallSummaryAnalyzer(BaseAnalyzer):
         )
         self.result.add(OptimizeRecord(optimization_item))
 
-        self.result.add_detail(const.BOTTLENECK, self.bottleneck_table["headers"], self.bottleneck_table["data"][0])
-        for data_type, data_dict in self.cur_data_table.items():
-            if data_dict:
-                self.result.add_detail(const.DATA + data_type, data_dict["headers"], data_dict["data"][0])
+        self.result.add_detail(
+            OverallSummaryAnalyzer.OVERALL_SUMMARY_ANALYZER,
+            headers=self.over_summary_analysis["headers"]
+        )
+        for data in self.over_summary_analysis["data"]:
+            self.result.add_detail(OverallSummaryAnalyzer.OVERALL_SUMMARY_ANALYZER, detail=data)
 
     def make_render(self):
         if not self.bottleneck_str and not self.cur_advices:
@@ -187,9 +207,8 @@ class OverallSummaryAnalyzer(BaseAnalyzer):
         result_for_html = {
             "Description": bottleneck_str,
             "suggestion": self.cur_advices,
-            "details": [self.bottleneck_table]
+            "details": [self.over_summary_analysis]
         }
-
         self.html_render.render_template(key="overall",
                                          title=OverallSummaryAnalyzer.OVERALL_SUMMARY_ANALYZER,
                                          template_dir="templates",
