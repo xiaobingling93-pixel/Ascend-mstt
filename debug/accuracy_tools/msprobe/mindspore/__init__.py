9ab00ace6c3bbdb588a7db4ff8a4cddc57d1d5f6
CSNIU <niuqirui@h-partners.com>
2024-08-01 20:05:41 +0800

    解决importerror错误

--- debug/accuracy_tools/msprobe/mindspore/__init__.py
+++ debug/accuracy_tools/msprobe/mindspore/__init__.py
@@ -1,4 +1,3 @@
 from msprobe.mindspore.debugger.precision_debugger import PrecisionDebugger
-from .common.utils import seed_all
 from .compare.acc_compare import compare
 from .compare.distributed_compare import compare_distributed
