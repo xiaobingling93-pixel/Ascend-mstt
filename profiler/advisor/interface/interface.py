75409358d6420e9ec63183fda5678b505f936198
zhaolei <zhaolei184@huawei.com>
2024-08-05 11:45:34 +0800

    1.小包检测

--- profiler/advisor/interface/interface.py
+++ profiler/advisor/interface/interface.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2024, Huawei Technologies Co., Ltd.
+# All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0  (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import os
 from collections import OrderedDict
 import sys
@@ -17,6 +31,7 @@ from profiler.advisor.analyzer.schedule.syncbn.syncbn_analyzer import SyncBNAnal
 from profiler.advisor.analyzer.schedule.synchronize_stream.synchronize_stream_analyzer import SynchronizeStreamAnalyzer
 from profiler.advisor.analyzer.dataloader.dataloader_analyzer import DataloaderAnalyzer
 from profiler.advisor.analyzer.computation.ai_core_freq.ai_core_freq_analyzer import AICoreFreqAnalyzer
+from profiler.advisor.analyzer.communication.packet_analyzer import PacketAnalyzer
 
 
 class Interface:
@@ -35,7 +50,9 @@ class Interface:
             SupportedScopes.GRAPH: FusionOPAnalyzer,
             SupportedScopes.FREQ_ANALYSIS: AICoreFreqAnalyzer
         }),
-        "communication": OrderedDict(),
+        "communication": OrderedDict({
+            SupportedScopes.PACKET: PacketAnalyzer
+        }),
         "overall": OrderedDict({SupportedScopes.OVER_ALL: OverallSummaryAnalyzer}),
         "dataloader": OrderedDict({SupportedScopes.DATALOADER: DataloaderAnalyzer}),
         "cluster": OrderedDict({
