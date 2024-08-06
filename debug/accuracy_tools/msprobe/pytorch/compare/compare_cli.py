8781b80be214a46828af172bbe09180cee3d54ec
Linwei-Ying <lwying007@126.com>
2024-08-06 10:30:08 +0800

    compare_distributed cli add

--- debug/accuracy_tools/msprobe/pytorch/compare/compare_cli.py
+++ debug/accuracy_tools/msprobe/pytorch/compare/compare_cli.py
@@ -2,6 +2,7 @@ import json
 from msprobe.core.common.file_check import FileOpen, check_file_type
 from msprobe.core.common.const import FileCheckConst
 from msprobe.core.common.utils import CompareException
+from msprobe.pytorch.common.log import logger
 from msprobe.pytorch.compare.acc_compare import compare
 from msprobe.pytorch.compare.distributed_compare import compare_distributed
 
@@ -15,7 +16,8 @@ def compare_cli(args):
         compare(input_param, args.output_path, stack_mode=args.stack_mode, auto_analyze=args.auto_analyze,
                 fuzzy_match=args.fuzzy_match)
     elif check_file_type(npu_path) == FileCheckConst.DIR and check_file_type(bench_path) == FileCheckConst.DIR:
-        compare_distributed(npu_path, bench_path, args.output_path)
+        kwargs = {"stack_mode": args.stack_mode, "auto_analyze": args.auto_analyze, "fuzzy_match": args.fuzzy_match}
+        compare_distributed(npu_path, bench_path, args.output_path, **kwargs)
     else:
         logger.error("The npu_path and bench_path need to be of the same type.")
         raise CompareException(CompareException.INVALID_COMPARE_MODE)
