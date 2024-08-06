812979fde516f779c3b68107ecc0296622abdbf9
h00613304 <hekunkun@huawei.com>
2024-08-06 20:05:01 +0800

    修复导入问题

--- debug/accuracy_tools/msprobe/pytorch/service.py
+++ debug/accuracy_tools/msprobe/pytorch/service.py
@@ -2,7 +2,7 @@ import functools
 import os
 from pathlib import Path
 
-import service
+import torch
 from msprobe.core.common.const import Const, FileCheckConst
 from msprobe.core.common.exceptions import DistributedNotInitializedError, MsprobeException
 from msprobe.core.common.file_check import FileChecker, check_path_before_create
