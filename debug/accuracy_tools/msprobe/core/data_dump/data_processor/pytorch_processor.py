9bb7e3e82725cf14b56cfa13de58173b0dce47f3
gitee <jiangchangting@huawei.com>
2024-08-08 11:41:10 +0800

    fix bug

--- debug/accuracy_tools/msprobe/core/data_dump/data_processor/pytorch_processor.py
+++ debug/accuracy_tools/msprobe/core/data_dump/data_processor/pytorch_processor.py
@@ -170,7 +170,7 @@ class TensorDataProcessor(PytorchDataProcessor):
     def _analyze_tensor(self, tensor, suffix):
         dump_data_name, file_path = self.get_save_file_path(suffix)
         saved_tensor = tensor.contiguous().detach()
-        torch.save(saved_tensor, file_path)
+        save_pt(saved_tensor, file_path)
         change_mode(file_path, FileCheckConst.DATA_FILE_AUTHORITY)
         single_arg = super()._analyze_tensor(tensor, suffix)
         single_arg.update({"data_name": dump_data_name})
