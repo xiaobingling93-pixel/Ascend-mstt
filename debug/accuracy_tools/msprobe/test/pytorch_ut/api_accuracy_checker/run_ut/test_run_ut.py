847cfa04564acafdb2b45605f347e58c21367faa
jiangchangting1 <jiangchangting@huawei.com>
2024-08-13 03:20:31 +0000

    update ut
    Signed-off-by: jiangchangting1 <jiangchangting@huawei.com>

--- debug/accuracy_tools/msprobe/test/pytorch_ut/api_accuracy_checker/run_ut/test_run_ut.py
+++ debug/accuracy_tools/msprobe/test/pytorch_ut/api_accuracy_checker/run_ut/test_run_ut.py
@@ -5,7 +5,7 @@ import unittest
 import torch
 from unittest.mock import patch, DEFAULT
 from msprobe.pytorch.api_accuracy_checker.run_ut.run_ut import *
-from msprobe.pytorch.api_accuracy_checker.common.utils import get_json_contents
+from msprobe.pytorch.common.utils import get_json_contents
 
 base_dir = os.path.dirname(os.path.realpath(__file__))
 forward_file = os.path.join(base_dir, "forward.json")
