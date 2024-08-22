b55d347e08a9edb803ce48e05463a400ef12760b
Linwei-Ying <lwying007@126.com>
2024-08-22 16:06:29 +0800

    add APIs in NPU and Bench models unconsistent judgment

--- debug/accuracy_tools/msprobe/core/compare/acc_compare.py
+++ debug/accuracy_tools/msprobe/core/compare/acc_compare.py
@@ -141,10 +141,7 @@ class Comparator:
                 continue
 
             # APIs in NPU and Bench models unconsistent judgment
-            if not npu_ops_queue and bench_ops_queue:
-                logger.info("Please check whether the number and calls of APIs in NPU and Bench models are consistent.")
-                break
-            elif npu_ops_queue and not bench_ops_queue:
+            if bool(npu_ops_queue) ^ bool(bench_ops_queue):
                 logger.info("Please check whether the number and calls of APIs in NPU and Bench models are consistent.")
                 break
 
