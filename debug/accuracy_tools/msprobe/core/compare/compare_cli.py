6a6743e9e15ff1cde7f441c2baf241c4b7e83cc9
jijiarong <jijiarong@huawei.com>
2024-08-22 16:50:19 +0800

    fix rank id

--- debug/accuracy_tools/msprobe/core/compare/compare_cli.py
+++ debug/accuracy_tools/msprobe/core/compare/compare_cli.py
@@ -37,7 +37,7 @@ def compare_cli(args):
             ms_compare(input_param, args.output_path, **kwargs)
     elif check_file_type(npu_path) == FileCheckConst.DIR and check_file_type(bench_path) == FileCheckConst.DIR:
         kwargs = {"stack_mode": args.stack_mode, "auto_analyze": auto_analyze, "fuzzy_match": args.fuzzy_match}
-        if input_param.get("rank_id") is None:
+        if input_param.get("rank_id") is not None:
             ms_graph_compare(input_param, args.output_path)
         if frame_name == Const.PT_FRAMEWORK:
             compare_distributed(npu_path, bench_path, args.output_path, **kwargs)
