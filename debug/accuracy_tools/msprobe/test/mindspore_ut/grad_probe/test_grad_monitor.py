75f215e0359fe7d47d6bd2053e25b16247101be2
qianzhengxin <paulqian29@163.com>
2024-09-02 17:01:59 +0800

    msprobe grad_probe ut

--- debug/accuracy_tools/msprobe/test/mindspore_ut/grad_probe/test_grad_monitor.py
+++ debug/accuracy_tools/msprobe/test/mindspore_ut/grad_probe/test_grad_monitor.py
@@ -4,11 +4,40 @@ import shutil
 import yaml
 from unittest import TestCase
 import hashlib
-from grad_tool.test.ut_ms.only_optimizer import main
+import mindspore
+from mindspore import nn, Tensor
+from mindspore.nn import SGD
+from grad_tool.grad_monitor import GradientMonitor
 
 
 file_path = os.path.abspath(__file__)
 directory = os.path.dirname(file_path)
+
+
+def main():
+    gm = GradientMonitor(os.path.join(directory, "config.yaml"), framework="MindSpore")
+
+    class SimpleNet(nn.Cell):
+        def __init__(self):
+            super().__init__()
+            self.my_dense = nn.Dense(16, 5)
+
+        def construct(self, x):
+            x = self.flatten(x)
+            logits = self.my_dense(x)
+            return logits
+    model = SimpleNet()
+    optimizer = SGD(model.trainable_params(), learning_rate=0.001)
+
+    gm.monitor(optimizer)
+
+    fix_gradient = tuple([Tensor(np.arange(5*16).reshape((5, 16)), dtype=mindspore.float32),
+                        Tensor(np.arange(5).reshape(5), dtype=mindspore.float32)])
+
+    steps = 10
+
+    for _ in range(steps):
+        optimizer(fix_gradient)
 
 
 def save_dict_as_yaml(data, yaml_file_path):
