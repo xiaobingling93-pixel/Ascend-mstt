54987e24946f7820a4854bf2d5689422e03b122f
wangqingcai <wqc01202410@gmail.com>
2024-09-04 10:25:22 +0800

    BaseLogger add debug level

--- debug/accuracy_tools/msprobe/core/common/const.py
+++ debug/accuracy_tools/msprobe/core/common/const.py
@@ -322,7 +322,8 @@ class MsgConst:
     Class for log messages const
     """
     CLEAR_SYMBOL = "\033[K"
-    LEVEL = ["INFO", "WARNING", "ERROR"]
+    LOG_LEVEL = "LOG_LEVEL"
+    LEVEL = ["INFO", "WARNING", "ERROR", "DEBUG"]
     SPECIAL_CHAR = ["\n", "\r", "\u007F", "\b", "\f", "\t", "\u000B", "%08", "%0a", "%0b", "%0c", "%0d", "%7f"]
 
 
