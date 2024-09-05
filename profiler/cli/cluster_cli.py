33305c62180c1e92fdc061999b861a71148991a2
zyy <zhouyiyan2@h-partners.com>
2024-09-05 19:58:31 +0800

    修改默认值

--- profiler/cli/cluster_cli.py
+++ profiler/cli/cluster_cli.py
@@ -34,7 +34,7 @@ context_settings['ignore_unknown_options'] = True
 @click.option('--profiling_path', '-d', type=click.Path(), required=True,
               help='path of the profiling data')
 @click.option('--mode', '-m', type=click.Choice(COMM_FEATURE_LIST), default='all')
-@click.option('--output_path', '-o', 'cluster_analysis_output_path', type=click.Path(), default='all',
+@click.option('--output_path', '-o', 'cluster_analysis_output_path', type=click.Path(), default='',
               help='Path of cluster analysis output')
 @click.argument('args', nargs=-1)
 def cluster_cli(profiling_path, mode, cluster_analysis_output_path, args) -> None:
