66194d444c3b60e84ef5991ce35662521933520e
wangqingcai <wqc01202410@gmail.com>
2024-09-23 20:48:16 +0800

    add ut

--- debug/accuracy_tools/msprobe/test/pytorch_ut/bench_functions/test_npu_fusion_attention.py
+++ debug/accuracy_tools/msprobe/test/pytorch_ut/bench_functions/test_npu_fusion_attention.py
@@ -1,7 +1,9 @@
+import pytest
 import torch
 import unittest
 
-from msprobe.pytorch.bench_functions.npu_fusion_attention import npu_fusion_attention, npu_fusion_attention_grad
+from msprobe.pytorch.bench_functions.npu_fusion_attention import npu_fusion_attention, npu_fusion_attention_grad, \
+    broadcast_kv
 
 
 class TestNpuFusionAttention(unittest.TestCase):
@@ -17,10 +19,17 @@ class TestNpuFusionAttention(unittest.TestCase):
         self.value = torch.randn(self.B, self.S2, self.N2, self.D)
         self.atten_mask = torch.randn(self.B, 1, self.S1, self.S2)
 
-    def test_basic_forward(self):
+    def test_basic_forward_input_layout_is_BSND(self):
         # 基本前向传播测试
         out, _, _ = npu_fusion_attention(self.query, self.key, self.value, head_num=self.N1, input_layout="BSND")
         self.assertEqual(out.shape, (self.B, self.S1, self.N1, self.D))
+
+    def test_basic_forward_input_layout_is_BNSD(self):
+        # 基本前向传播测试
+        self.query = torch.randn(self.B, self.N1, self.S1, self.D)
+        self.key = torch.randn(self.B, self.N2, self.S2, self.D)
+        out, _, _ = npu_fusion_attention(self.query, self.key, self.value, head_num=self.N1, input_layout="BNSD")
+        self.assertEqual(out.shape, (self.B, self.N1, self.S1, self.D))
 
     def test_basic_backward(self):
         # 基本反向传播测试
@@ -29,12 +38,19 @@ class TestNpuFusionAttention(unittest.TestCase):
         self.assertEqual(out.shape, (self.B,  self.S1, self.N1, self.D))  # 检查dq形状
 
     def test_different_input_layout(self):
-        # 不同输入布局测试
+        # BSH
         self.query = torch.randn(self.B, self.S1, self.N1*self.D)
         self.key = torch.randn(self.B, self.S2, self.N2*self.D)
         self.value = torch.randn(self.B, self.S2, self.N2*self.D)
         out, _, _ = npu_fusion_attention(self.query, self.key, self.value, head_num=self.N1, input_layout="BSH")
         self.assertEqual(out.shape, (self.B, self.S1, self.N1 * self.D))
+
+        # SBH
+        self.query = torch.randn(self.S1, self.B, self.N1*self.D)
+        self.key = torch.randn(self.S2, self.B, self.N2*self.D)
+        self.value = torch.randn(self.S2, self.B, self.N2*self.D)
+        out, _, _ = npu_fusion_attention(self.query, self.key, self.value, head_num=self.N1, input_layout="SBH")
+        self.assertEqual(out.shape, (self.S1, self.B, self.N1 * self.D))
 
     def test_with_attention_mask(self):
         # 带注意力掩码的测试
@@ -56,3 +72,35 @@ class TestNpuFusionAttention(unittest.TestCase):
         self.key = torch.randn(self.B, self.N1 + 1, self.S2, self.D)  # 故意制造维度不匹配
         with self.assertRaises(ValueError):
             npu_fusion_attention(self.query, self.key, self.value, head_num=self.N1, input_layout="BSND")
+
+    def test_input_layout_is_TND(self):
+        with self.assertRaises(ValueError):
+            npu_fusion_attention(self.query, self.key, self.value, head_num=self.N1, input_layout="TND")
+
+    def test_broadcast_kv(self):
+        # valid input
+        num_heads = 4
+        num_kv_heads = 2
+        kv_tensor = torch.randn(1, num_kv_heads, 10, 10)
+        result = broadcast_kv(num_heads, num_kv_heads, kv_tensor, torch.float32)
+        self.assertEqual(result.shape, (1, num_heads, 10, 10))
+
+        # invalid_input
+        num_heads = 4
+        kv_tensor = torch.randn(1, num_kv_heads, 10, 10)
+        num_kv_heads = 0
+        with pytest.raises(ValueError):
+            broadcast_kv(num_heads, num_kv_heads, kv_tensor, torch.float32)
+        num_kv_heads = 5
+        with pytest.raises(ValueError):
+            broadcast_kv(num_heads, num_kv_heads, kv_tensor, torch.float32)
+
+        # num_heads equals to num_kv_heads
+        num_heads = 4
+        num_kv_heads = 4
+        kv_tensor = torch.randn(1, num_kv_heads, 10, 10)
+        result = broadcast_kv(num_heads, num_kv_heads, kv_tensor, torch.float32)
+
+        self.assertEqual(result.shape, (1, num_heads, 10, 10))
+        self.assertEqual(result.dtype, torch.float32)
+        self.assertTrue(torch.allclose(result, kv_tensor.expand(-1, num_heads, -1, -1)))
