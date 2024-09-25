a5ae1c507fbea356e2b9758659a05e16c0d67859
zhaolei <zhaolei184@huawei.com>
2024-09-25 10:25:51 +0800

    advisor setup.py生成wheel包失败bug修复

--- profiler/advisor/config/config.py
+++ profiler/advisor/config/config.py
@@ -4,15 +4,10 @@ advisor config
 
 import logging
 import os
-import configparser
-from urllib.parse import urlparse
-from email.utils import parseaddr
-from typing import Dict, List
 
 from profiler.advisor.utils.utils import Timer
 from profiler.advisor.utils.utils import singleton
-from profiler.prof_common.path_manager import PathManager
-from profiler.advisor.utils.utils import convert_to_float
+from profiler.prof_common.utils import SafeConfigReader
 
 logger = logging.getLogger()
 
@@ -42,7 +37,7 @@ class Config:
         self._work_path = os.getcwd()  # pwd
         self._root_path = os.path.abspath(os.path.join(__file__, "../../"))
         self.config_reader = SafeConfigReader(os.path.join(self._root_path, self._CONFIG_DIR_NAME,
-                                                              self._CONFIG_FILE_NAME))
+                                                           self._CONFIG_FILE_NAME))
         self.config_reader.validate(self._REQUIRED_SECTIONS)
         self.config = self.config_reader.get_config()
         # ANALYSE
@@ -173,52 +168,4 @@ class Config:
         if self.log_path and os.path.isdir(self.log_path) and not os.listdir(self.log_path):
             os.rmdir(self.log_path)
 
-
-class SafeConfigReader:
-    def __init__(self, config_file):
-        self._validation_mapping = {
-            'THRESHOLD': self.check_threshold,
-            'URL': self.check_url,
-            'EMAIL': self.check_email
-        }
-        self._config = configparser.RawConfigParser()
-        self.read_config(config_file)
-
-    def read_config(self, path):
-        PathManager.check_input_file_path(path)
-        PathManager.check_path_readable(path)
-        PathManager.check_file_size(path)
-        self._config.read(path)
-
-    def get_config(self):
-        return self._config
-
-    def validate(self, required_sections: Dict = dict):
-        for section, keys in required_sections.items():
-            if section not in self._config:
-                raise ValueError(f"Missing required section: {section}")
-            if self._validation_mapping.get(section, None):
-                self._validation_mapping.get(section)(section, keys)
-            for key in keys:
-                if key not in self._config[section]:
-                    raise ValueError(f"Missing required key '{key}' in section '{section}'.")
-
-    def check_threshold(self, section, keys: List):
-        for key in keys:
-            value = convert_to_float(self._config.get(section, key))
-            if value < 0 or value > 1:
-                raise ValueError("Threshold %s is not between 0 and 1", value)
-
-    def check_url(self, section, keys: List):
-        for key in keys:
-            url = self._config.get(section, key)
-            parsed_url = urlparse(url)
-            if not all([parsed_url.scheme, parsed_url.netloc]):
-                raise ValueError("url %s is not valid", url)
-
-    def check_email(self, section, keys: List):
-        for key in keys:
-            email = self._config.get(section, key)
-            if '@' not in parseaddr(email)[1]:  # parseaddr固定返回一个双元组，无越界风险
-                raise ValueError("email %s is not valid", email)
 
