121c2ff4b4d513897db509f05244563570a75eb3
gitee <jiangchangting@huawei.com>
2024-09-26 09:42:55 +0800

    add ut

--- debug/accuracy_tools/msprobe/test/pytorch_ut/api_accuracy_checker/compare/test_compare_utils.py
+++ debug/accuracy_tools/msprobe/test/pytorch_ut/api_accuracy_checker/compare/test_compare_utils.py
@@ -1,12 +1,43 @@
 import unittest
 
+import torch
 import numpy as np
 
 from msprobe.pytorch.api_accuracy_checker.common.utils import CompareException
-from msprobe.pytorch.api_accuracy_checker.compare.compare_utils import check_dtype_comparable, convert_str_to_float
+from msprobe.pytorch.api_accuracy_checker.compare.compare_utils import *
 
 
 class TestCompareUtils(unittest.TestCase):
+    
+    def setUp(self):
+        self.column = ApiPrecisionCompareColumn()
+    
+    def test_get_detail_csv_title(self):
+        expect_detail_csv_title = [
+                self.column.API_NAME, self.column.SMALL_VALUE_ERROR_RATIO, 
+                self.column.SMALL_VALUE_ERROR_STATUS, self.column.RMSE_RATIO, 
+                self.column.RMSE_STATUS, self.column.MAX_REL_ERR_RATIO, 
+                self.column.MAX_REL_ERR_STATUS, self.column.MEAN_REL_ERR_RATIO, 
+                self.column.MEAN_REL_ERR_STATUS, self.column.EB_RATIO, 
+                self.column.EB_STATUS, self.column.INF_NAN_ERROR_RATIO, 
+                self.column.INF_NAN_ERROR_RATIO_STATUS, self.column.REL_ERR_RATIO, 
+                self.column.REL_ERR_RATIO_STATUS, self.column.ABS_ERR_RATIO, 
+                self.column.ABS_ERR_RATIO_STATUS, self.column.ERROR_RATE, 
+                self.column.ERROR_RATE_STATUS, self.column.MEAN_ULP_ERR, 
+                self.column.ULP_ERR_PROPORTION, self.column.ULP_ERR_PROPORTION_RATIO, 
+                self.column.ULP_ERR_STATUS, self.column.REL_ERR_THOUSANDTH, 
+                self.column.REL_ERR_THOUSANDTH_STATUS, self.column.FINAL_RESULT, 
+                self.column.ALGORITHM, self.column.MESSAGE
+                ]
+        self.assertEqual(self.column.get_detail_csv_title(), expect_detail_csv_title)
+    
+    def test_get_result_csv_title(self):
+        expect_result_csv_title =[
+                self.column.API_NAME, self.column.FORWWARD_STATUS, 
+                self.column.BACKWARD_STATUS, self.column.MESSAGE
+                ]
+        self.assertEqual(self.column.get_result_csv_title(), expect_result_csv_title)
+
     def test_check_dtype_comparable(self):
         x = np.array([1, 2, 3], dtype=np.int32)
         y = np.array([4, 5, 6], dtype=np.int32)
@@ -23,9 +54,21 @@ class TestCompareUtils(unittest.TestCase):
         x = np.array([1, 2, 3], dtype=np.int32)
         y = np.array([4.0, 5.0, 6.0], dtype=np.float32)
         self.assertFalse(check_dtype_comparable(x, y))
+        
+        x = np.array([4.0, 5.0, 6.0], dtype=np.float32)
+        y = np.array([1, 2, 3], dtype=np.int32)
+        self.assertFalse(check_dtype_comparable(x, y))
 
         x = np.array([1, 2, 3], dtype=np.int32)
         y = np.array([True, False, True], dtype=np.bool_)
+        self.assertFalse(check_dtype_comparable(x, y))
+        
+        x = np.array([True, False, True], dtype=np.bool_)
+        y = np.array([1, 2, 3], dtype=np.int32)
+        self.assertFalse(check_dtype_comparable(x, y))
+        
+        x = torch.tensor([4.0, 5.0, 6.0], dtype=torch.float32)
+        y = np.array([1, 2, 3], dtype=np.int32)
         self.assertFalse(check_dtype_comparable(x, y))
 
     def test_convert_str_to_float_when_valid_float(self):
@@ -41,3 +84,51 @@ class TestCompareUtils(unittest.TestCase):
         with self.assertRaises(CompareException) as cm:
             convert_str_to_float('')
         self.assertEqual(cm.exception.code, CompareException.INVALID_DATA_ERROR)
+        
+    def test_convert_str_to_float_wiht_whitespace_string(self):
+        with self.assertRaises(CompareException) as context:
+            convert_str_to_float('    ')
+        self.assertEqual(context.exception.error_code, CompareException.INVALID_DATA_ERROR)
+        
+    def test_handle_infinity_both_inf_same_sign(self):
+        result, consistent, message = handle_infinity(float('inf'), float('inf'), 'test_column')
+        self.assertTrue(math.isnan(result))
+        self.assertTrue(consistent)
+        self.assertEqual(message, "test_column同为同号inf或nan\n")
+
+    def test_handle_infinity_both_inf_different_sign(self):
+        result, consistent, message = handle_infinity(float('inf'), float('-inf'), 'test_column')
+        self.assertTrue(math.isnan(result))
+        self.assertFalse(consistent)
+        self.assertEqual(message, "test_columninf或nan不一致\n")
+
+    def test_handle_infinity_one_inf(self):
+        result, consistent, message = handle_infinity(float('inf'), 1.0, 'test_column')
+        self.assertTrue(math.isnan(result))
+        self.assertFalse(consistent)
+        self.assertEqual(message, "test_columninf或nan不一致\n")
+
+    def test_handle_nan_both_nan(self):
+        result, consistent, message = handle_nan(float('nan'), float('nan'), 'test_column')
+        self.assertTrue(math.isnan(result))
+        self.assertTrue(consistent)
+        self.assertEqual(message, "test_column同为同号inf或nan\n")
+
+    def test_handle_nan_one_nan(self):
+        result, consistent, message = handle_nan(float('nan'), 1.0, 'test_column')
+        self.assertTrue(math.isnan(result))
+        self.assertFalse(consistent)
+        self.assertEqual(message, "test_columninf或nan不一致\n")
+
+    def test_check_inf_or_nan_both_inf(self):
+        result, consistent, message = check_inf_or_nan(float('inf'), float('inf'), 'test_column')
+        self.assertTrue(math.isnan(result))
+        self.assertTrue(consistent)
+        self.assertEqual(message, "test_column同为同号inf或nan\n")
+
+    def test_check_inf_or_nan_both_nan(self):
+        # 测试两个值都是 NaN
+        result, consistent, message = check_inf_or_nan(float('nan'), float('nan'), 'test_column')
+        self.assertTrue(math.isnan(result))
+        self.assertTrue(consistent)
+        self.assertEqual(message, "test_column同为同号inf或nan\n")
