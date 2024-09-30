c9348840f17aa482572b495ee8d64b8a12bad13f
jiangchao_j <chaojiang_j@163.com>
2024-09-30 17:33:00 +0800

    add mindspore free_bechmark ut p2

---
+++ debug/accuracy_tools/msprobe/test/mindspore_ut/free_benchmark/test_ms_self_check_tool_factory.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+# Copyright (C) 2024-2024. Huawei Technologies Co., Ltd. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+
+import os
+import unittest
+
+from msprobe.mindspore.free_benchmark.self_check_tool_factory import SelfCheckToolFactory
+from msprobe.mindspore.free_benchmark.api_pynative_self_check import ApiPyNativeSelFCheck
+from msprobe.mindspore.debugger.debugger_config import DebuggerConfig
+from msprobe.core.common_config import CommonConfig, BaseConfig
+from msprobe.mindspore.common.const import Const as MsConst
+from msprobe.core.common.const import Const
+
+
+class TestSelfCheckToolFactory(unittest.TestCase):
+
+    def test_create(self):
+        common_config = CommonConfig({})
+        common_config.task = Const.FREE_BENCHMARK
+        common_config.dump_path = os.path.dirname(os.path.realpath(__file__))
+        task_config = BaseConfig({})
+        config = DebuggerConfig(common_config, task_config)
+
+        config.level = "UNKNOWN"
+        with self.assertRaises(Exception) as context:
+            SelfCheckToolFactory.create(config)
+        self.assertEqual(str(context.exception), "UNKNOWN is not supported.")
+
+        config.level = MsConst.API
+        config.execution_mode = MsConst.GRAPH_KBYK_MODE
+        with self.assertRaises(Exception) as context:
+            SelfCheckToolFactory.create(config)
+        self.assertEqual(str(context.exception),
+                         f"Task free_benchmark is not supported in this mode: {MsConst.GRAPH_KBYK_MODE}.")
+
+        config.execution_mode = MsConst.PYNATIVE_MODE
+        tool = SelfCheckToolFactory.create(config)
+        self.assertTrue(isinstance(tool, ApiPyNativeSelFCheck))
