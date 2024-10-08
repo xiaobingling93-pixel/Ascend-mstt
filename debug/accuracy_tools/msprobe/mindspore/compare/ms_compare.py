2761c03e86a0485c3da2c93b2df718766751e4cc
CSNIU <niuqirui@h-partners.com>
2024-10-08 19:13:20 +0800

    拆解PR，先提交非版权部分

--- debug/accuracy_tools/msprobe/mindspore/compare/ms_compare.py
+++ debug/accuracy_tools/msprobe/mindspore/compare/ms_compare.py
@@ -1,17 +1,3 @@
-# Copyright (c) 2024-2024, Huawei Technologies Co., Ltd.
-# All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0  (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 import os
 import copy
 import sys
@@ -136,11 +122,8 @@ class MSComparator(Comparator):
         del summary[idx]
     
     def get_api_name(self, api_list):
-        if len(api_list) > 2:
-            return api_list[0] + Const.SEP + api_list[1]
-        else:
-            logger.error("Failed to retrieve API name, please check if the downloaded data is reasonable")
-            raise Exception("Failed to retrieve API name, please check if the downloaded data is reasonable")
+        return api_list[0] + Const.SEP + api_list[1]
+
     
     def transform_user_mapping_api(self, new_npu_dict, new_bench_dict):
         """
