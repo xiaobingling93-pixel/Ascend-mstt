84c9fa19fee82f5b7e792598dca46400f8b72bda
Linwei-Ying <lwying007@126.com>
2024-10-10 09:28:24 +0800

    compare ut add pt_compare.py

--- debug/accuracy_tools/msprobe/test/pytorch_ut/compare/test_pt_compare.py
+++ debug/accuracy_tools/msprobe/test/pytorch_ut/compare/test_pt_compare.py
@@ -1,7 +1,27 @@
 # coding=utf-8
+import os
+import torch
 import unittest
+import numpy as np
 from msprobe.pytorch.compare.pt_compare import PTComparator
+from msprobe.core.common.utils import CompareException
 from msprobe.test.core_ut.compare.test_acc_compare import npu_dict, bench_dict
+
+
+base_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), f'test_pt_compare')
+
+
+def generate_npy(base_dir):
+    data_path = os.path.join(base_dir, '1.npy')
+    data = np.array([1, 2, 3, 4])
+    np.save(data_path, data)
+
+
+def generate_pt(base_dir):
+    data_path = os.path.join(base_dir, '1.pt')
+    data = torch.Tensor([1, 2, 3, 4])
+    torch.save(data, data_path)
+
 
 class TestUtilsMethods(unittest.TestCase):
 
@@ -18,3 +38,8 @@ class TestUtilsMethods(unittest.TestCase):
         self.assertEqual(a, 0)
         self.assertEqual(b, 0)
 
+    def test_read_npy_data(self):
+        generate_npy(base_dir)
+        with self.assertRaises(CompareException) as context:
+            result = PTComparator().read_npy_data(base_dir, '1.npy')
+        self.assertEqual(context.exception.code, CompareException(CompareException.INVALID_FILE_ERROR))
