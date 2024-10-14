a6bfc19cebcb40454dab57fa5a9d66fcceb9ce3c
hehongzhe <hehongzhe@h-partners.com>
2024-10-14 16:45:28 +0800

    马建仓优化修改

--- profiler/test/st/cluster_analyse/test_cluster_analyse_cmd_pytorch_text.py
+++ profiler/test/st/cluster_analyse/test_cluster_analyse_cmd_pytorch_text.py
@@ -14,33 +14,20 @@ class TestClusterAnalyseCmdPytorchText(TestCase):
     OUTPUT_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 
                                "ClusterAnalyseCmdPytorchText")
     OUTPUT_DATA = ""
-    RESULT_CSV = "cluster_step_trace_time.csv"
-    RESULT_BANDWIDTH = "cluster_communication_matrix.json"
-    RESULT_COMMUNICATION = "cluster_communication.json"
+    RESULT_FILES = {
+        "csv":"cluster_step_trace_time.csv",
+        "bandwidth":"cluster_communication_matrix.json",
+        "communication":"cluster_communication.json",
+    }
     COMMAND_SUCCESS = 0
 
     def setup_class(self):
         # make a temporary directory for analysis results
         PathManager.make_dir_safety(self.OUTPUT_PATH)
-        
-        cmd = ["msprof-analyze", "cluster", "-d", self.CLUSTER_PATH, "-m", "all",
-               "--output_path", self.OUTPUT_PATH]
-        completed_process = subprocess.run(cmd, capture_output = True, shell = False)
-        self.OUTPUT_DATA = os.path.join(self.OUTPUT_PATH, "cluster_analysis_output")
-        if (completed_process.returncode != self.COMMAND_SUCCESS 
-            or not os.path.exists(self.OUTPUT_DATA)):
-            self.assertEqual(False, True, msg = "pytorch text cluster analyse task failed.")
-        
-        files = os.listdir(self.OUTPUT_DATA)
-        if self.RESULT_CSV not in files:
-            self.assertEqual(False, True, 
-                             msg = f"PyTorch text result {self.RESULT_CSV} not find.")
-        if self.RESULT_BANDWIDTH not in files:
-            self.assertEqual(False, True, 
-                             msg = f"PyTorch text result {self.RESULT_BANDWIDTH} not find.")
-        if self.RESULT_COMMUNICATION not in files:
-            self.assertEqual(False, True, 
-                             msg = f"PyTorch text result {self.RESULT_COMMUNICATION} not find.")
+        # test msprof-analyze cmd
+        self.run_analyse()
+        # make sure result files exist
+        self.check_expect()
     
     def teardown_class(self):
         # remove temporary directory
@@ -50,11 +37,28 @@ class TestClusterAnalyseCmdPytorchText(TestCase):
         """
         Check cluster_step_trace_time.csv result right
         """
-        df = pd.read_csv(os.path.join(self.OUTPUT_DATA, self.RESULT_CSV), sep = ',', header = 0)
-        headers = ["Step", "Type", "Index", "Computing", "Communication(Not Overlapped)", "Overlapped", "Communication", "Free", 
-                   "Stage", "Bubble", "Communication(Not Overlapped and Exclude Receive)", "Preparing"]
+        df = pd.read_csv(os.path.join(self.OUTPUT_DATA, self.RESULT_FILES["csv"]), sep = ',', header = 0)
+        headers = ["Step", "Type", "Index", "Computing", "Communication(Not Overlapped)",
+                   "Overlapped", "Communication", "Free", "Stage", "Bubble",
+                   "Communication(Not Overlapped and Exclude Receive)", "Preparing"]
         self.assertEqual(headers, df.columns.tolist(), "PyTorch text result columns wrong.")
 
         data_base = ["rank", "7", 14945901.573999925, 50289541.49199608, 14462809.01400388, 64752350.50599996, 
                      377397.078000026, 65612840.25, 0.0, 50289541.49199608, 1726054679554437.8]
         self.assertEqual(data_base, df.iloc[0].loc["Type":"Preparing"].tolist(), "PyTorch text result data wrong.")
+
+    def run_analyse(self):
+        cmd = ["msprof-analyze", "cluster", "-d", self.CLUSTER_PATH, "-m", "all",
+               "--output_path", self.OUTPUT_PATH]
+        completed_process = subprocess.run(cmd, capture_output=True, shell=False)
+        self.OUTPUT_DATA = os.path.join(self.OUTPUT_PATH, "cluster_analysis_output")
+        if (completed_process.returncode != self.COMMAND_SUCCESS
+                or not os.path.exists(self.OUTPUT_DATA)):
+            self.assertEqual(False, True, msg="pytorch text cluster analyse task failed.")
+
+    def check_expect(self):
+        files = os.listdir(self.OUTPUT_DATA)
+        for file_name in self.RESULT_FILES.values():
+            if file_name not in files:
+                self.assertEqual(False, True,
+                                 msg=f"PyTorch text result {file_name} not find.")
