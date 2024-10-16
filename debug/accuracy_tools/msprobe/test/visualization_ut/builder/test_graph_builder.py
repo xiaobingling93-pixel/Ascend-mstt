f369c3b0b72f98dcb243619c99ff9a1b71e5b589
wugengjun <451676383@qq.com>
2024-10-16 15:06:45 +0800

    精度可视化工具ut搭建

---
+++ debug/accuracy_tools/msprobe/test/visualization_ut/builder/test_graph_builder.py
@@ -0,0 +1,99 @@
+import unittest
+from unittest.mock import MagicMock, patch
+from msprobe.visualization.builder.graph_builder import GraphBuilder, Graph, BaseNode, NodeOp
+
+
+class TestGraphBuilder(unittest.TestCase):
+
+    def setUp(self):
+        self.construct_path = "step/rank/construct.json"
+        self.data_path = "step/rank/dump.json"
+        self.model_name = "TestModel"
+        self.graph = Graph(self.model_name)
+        self.construct_dict = {
+            "Tensor1": "Module1",
+            "Module1": None
+        }
+        self.data_dict = {
+            "Module1": {"data": "data for Module1"},
+            "Tensor1": {"data": "data for Tensor1"}
+        }
+
+    @patch('msprobe.visualization.builder.graph_builder.load_json_file')
+    @patch('msprobe.visualization.builder.graph_builder.load_data_json_file')
+    def test_build(self, mock_load_data_json_file, mock_load_json_file):
+        mock_load_data_json_file.return_value = self.data_dict
+        mock_load_json_file.return_value = self.construct_dict
+
+        graph = GraphBuilder.build(self.construct_path, self.data_path, self.model_name)
+        self.assertIsNotNone(graph)
+        self.assertIsInstance(graph, Graph)
+        self.assertEqual(len(graph.node_map), 3)
+
+    @patch('msprobe.visualization.builder.graph_builder.save_json_file')
+    def test_to_json(self, mock_save_json_file):
+        GraphBuilder.to_json("step/rank/output.vis", self.graph)
+        mock_save_json_file.assert_called_once()
+
+    @patch('msprobe.visualization.graph.node_op.NodeOp.get_node_op')
+    @patch('msprobe.visualization.builder.msprobe_adapter.get_input_output', return_value=([], []))
+    def test__init_nodes(self, mock_get_input_output, mock_get_node_op):
+        GraphBuilder._init_nodes(self.graph, self.construct_dict, self.data_dict)
+        mock_get_node_op.assert_any_call("Tensor1")
+        mock_get_node_op.assert_any_call("Module1")
+        self.assertIs(self.graph.root, self.graph.get_node("TestModel"))
+
+    def test__create_or_get_node(self):
+        node_op = MagicMock()
+        data_dict = {"node1": {}}
+        node = GraphBuilder._create_or_get_node(self.graph, data_dict, node_op, "node1")
+        self.assertIn("node1", self.graph.node_map)
+        self.assertEqual(node.input_data, {})
+        self.assertEqual(node.output_data, {})
+
+    def test__handle_backward_upnode_missing(self):
+        construct_dict = {'Module.module.a.forward.0': 'Module.root.forward.0', 'Module.module.a.backward.0': None,
+                          'Module.root.forward.0': None, 'Module.root.backward.0': None,
+                          'Module.module.b.forward.0': 'Module.root.forward.0',
+                          'Module.module.b.backward.0': 'Module.root.backward.0', 'Module.module.c.backward.0': None}
+        node_id_a = GraphBuilder._handle_backward_upnode_missing(construct_dict, 'Module.module.a.backward.0', None)
+        self.assertEqual(node_id_a, 'Module.root.backward.0')
+        node_id_b = GraphBuilder._handle_backward_upnode_missing(construct_dict, 'Module.module.b.backward.0',
+                                                                 'Module.root.backward.0')
+        self.assertEqual(node_id_b, 'Module.root.backward.0')
+        node_id_c = GraphBuilder._handle_backward_upnode_missing(construct_dict, 'Module.module.c.backward.0', None)
+        self.assertIsNone(node_id_c)
+
+    def test__collect_apis_between_modules_only_apis(self):
+        graph = Graph('TestNet')
+        graph.root.subnodes = [BaseNode(NodeOp.function_api, 'Tensor.a.0'), BaseNode(NodeOp.function_api, 'Tensor.b.0')]
+        GraphBuilder._collect_apis_between_modules(graph)
+        self.assertEqual(len(graph.root.subnodes), 1)
+        self.assertEqual(graph.root.subnodes[0].op, NodeOp.api_collection)
+        self.assertEqual(len(graph.root.subnodes[0].subnodes), 2)
+        self.assertEqual(graph.root.subnodes[0].id, 'Apis_Between_Modules.0')
+
+    def test__collect_apis_between_modules_mixed_nodes(self):
+        graph = Graph('TestNet')
+        graph.root.subnodes = [BaseNode(NodeOp.function_api, 'Tensor.a.0'), BaseNode(NodeOp.module, 'Module.a.0'),
+                               BaseNode(NodeOp.module, 'Module.b.0'), BaseNode(NodeOp.function_api, 'Tensor.b.0'),
+                               BaseNode(NodeOp.function_api, 'Tensor.c.0'), BaseNode(NodeOp.module, 'Module.a.1')]
+        GraphBuilder._collect_apis_between_modules(graph)
+        self.assertEqual(len(graph.root.subnodes), 5)
+        self.assertEqual(graph.root.subnodes[0].op, NodeOp.function_api)
+        self.assertEqual(graph.root.subnodes[1].op, NodeOp.module)
+        self.assertEqual(graph.root.subnodes[3].op, NodeOp.api_collection)
+        self.assertEqual(len(graph.root.subnodes[3].subnodes), 2)
+        self.assertEqual(graph.root.subnodes[3].id, 'Apis_Between_Modules.0')
+
+    def test__collect_apis_between_modules_only_modules(self):
+        graph = Graph('TestNet')
+        graph.root.subnodes = [BaseNode(NodeOp.module, 'Module.a.0'), BaseNode(NodeOp.module, 'Module.b.0'),
+                               BaseNode(NodeOp.module, 'Module.a.1')]
+        GraphBuilder._collect_apis_between_modules(graph)
+        self.assertEqual(len(graph.root.subnodes), 3)
+        self.assertEqual(graph.root.subnodes[0].op, NodeOp.module)
+        self.assertEqual(graph.root.subnodes[1].op, NodeOp.module)
+        self.assertEqual(graph.root.subnodes[2].op, NodeOp.module)
+        self.assertEqual(len(graph.root.subnodes[0].subnodes), 0)
+        self.assertEqual(graph.root.subnodes[0].id, 'Module.a.0')
---
+++ debug/accuracy_tools/msprobe/test/visualization_ut/compare/test_mode_adapter.py
@@ -0,0 +1,99 @@
+import json
+import unittest
+from unittest.mock import patch, MagicMock
+from msprobe.visualization.compare.mode_adapter import ModeAdapter
+from msprobe.visualization.graph.base_node import BaseNode, NodeOp
+from msprobe.visualization.utils import GraphConst, ToolTip
+from msprobe.core.common.const import CompareConst
+
+
+class TestModeAdapter(unittest.TestCase):
+
+    def setUp(self):
+        self.node_op = NodeOp.module
+        self.node_id = "node_1"
+        self.node = BaseNode(self.node_op, self.node_id)
+        self.compare_mode = GraphConst.REAL_DATA_COMPARE
+        self.adapter = ModeAdapter(self.compare_mode)
+        self.compare_data_dict = [{}, {}]
+
+    def test_add_md5_compare_data(self):
+        node_data = {'md5_key': 'some_md5_value'}
+        compare_data_dict = {'md5_key': 'expected_md5_value'}
+        precision_index = ModeAdapter._add_md5_compare_data(node_data, compare_data_dict)
+        self.assertEqual(precision_index, 0)
+
+    @patch('msprobe.visualization.compare.mode_adapter.ModeAdapter')
+    def test_parse_result(self, mock_mode_adapter):
+        mock_mode_adapter._add_summary_compare_data.return_value = 0.5
+        self.adapter.compare_mode = GraphConst.SUMMARY_COMPARE
+        precision_index, other_dict = self.adapter.parse_result(self.node, self.compare_data_dict)
+        self.assertEqual(precision_index, 0.5)
+        self.assertEqual(other_dict, {})
+
+        mock_mode_adapter._add_md5_compare_data.return_value = 1
+        self.adapter.compare_mode = GraphConst.MD5_COMPARE
+        precision_index, other_dict = self.adapter.parse_result(self.node, self.compare_data_dict)
+        self.assertEqual(precision_index, 1)
+        self.assertEqual(other_dict, {'Result': 'pass'})
+
+        mock_mode_adapter._add_real_compare_data.return_value = 0.6
+        self.adapter.compare_mode = GraphConst.REAL_DATA_COMPARE
+        precision_index, other_dict = self.adapter.parse_result(self.node, self.compare_data_dict)
+        self.assertEqual(precision_index, 0.0)
+        self.assertEqual(other_dict, {})
+
+    def test_prepare_real_data(self):
+        self.adapter.is_real_data_compare = MagicMock(return_value=True)
+        result = self.adapter.prepare_real_data(self.node)
+        self.assertTrue(result)
+
+        self.adapter.is_real_data_compare = MagicMock(return_value=False)
+        result = self.adapter.prepare_real_data(self.node)
+        self.assertFalse(result)
+
+    def test_compare_mode_methods(self):
+        self.adapter.compare_mode = GraphConst.SUMMARY_COMPARE
+        self.assertTrue(self.adapter.is_summary_compare())
+        self.assertFalse(self.adapter.is_md5_compare())
+        self.assertFalse(self.adapter.is_real_data_compare())
+
+    def test_add_csv_data(self):
+        compare_result_list = ['result1', 'result2']
+        self.adapter.add_csv_data(compare_result_list)
+        self.assertEqual(self.adapter.csv_data, compare_result_list)
+
+    def test_add_error_key(self):
+        node_data = {'key': {}}
+        self.adapter.compare_mode = GraphConst.REAL_DATA_COMPARE
+        self.adapter.add_error_key(node_data)
+        self.assertEqual(node_data['key'][GraphConst.ERROR_KEY],
+                         [CompareConst.ONE_THOUSANDTH_ERR_RATIO, CompareConst.FIVE_THOUSANDTHS_ERR_RATIO])
+        node_data = {'key': {}}
+        self.adapter.compare_mode = GraphConst.SUMMARY_COMPARE
+        self.adapter.add_error_key(node_data)
+        self.assertEqual(node_data['key'][GraphConst.ERROR_KEY],
+                         [CompareConst.MAX_RELATIVE_ERR, CompareConst.MIN_RELATIVE_ERR,
+                          CompareConst.MEAN_RELATIVE_ERR, CompareConst.NORM_RELATIVE_ERR])
+
+    def test_get_tool_tip(self):
+        self.adapter.compare_mode = GraphConst.MD5_COMPARE
+        tips = self.adapter.get_tool_tip()
+        self.assertEqual(tips, json.dumps({'md5': ToolTip.MD5}))
+
+        self.adapter.compare_mode = GraphConst.SUMMARY_COMPARE
+        tips = self.adapter.get_tool_tip()
+        self.assertEqual(tips, json.dumps({
+            CompareConst.MAX_DIFF: ToolTip.MAX_DIFF,
+            CompareConst.MIN_DIFF: ToolTip.MIN_DIFF,
+            CompareConst.MEAN_DIFF: ToolTip.MEAN_DIFF,
+            CompareConst.NORM_DIFF: ToolTip.NORM_DIFF}))
+
+        self.adapter.compare_mode = GraphConst.REAL_DATA_COMPARE
+        tips = self.adapter.get_tool_tip()
+        self.assertEqual(tips, json.dumps({
+            CompareConst.ONE_THOUSANDTH_ERR_RATIO: ToolTip.ONE_THOUSANDTH_ERR_RATIO,
+            CompareConst.FIVE_THOUSANDTHS_ERR_RATIO: ToolTip.FIVE_THOUSANDTHS_ERR_RATIO,
+            CompareConst.COSINE: ToolTip.COSINE,
+            CompareConst.MAX_ABS_ERR: ToolTip.MAX_ABS_ERR,
+            CompareConst.MAX_RELATIVE_ERR: ToolTip.MAX_RELATIVE_ERR}))
