f5993153acadfaf9f790838d103e3bee571f6473
Linwei-Ying <lwying007@126.com>
2024-10-18 16:18:45 +0800

    online_dispatch ut add utils.py

--- debug/accuracy_tools/msprobe/test/pytorch_ut/online_dispatch/test_utils_online_dispatch.py
+++ debug/accuracy_tools/msprobe/test/pytorch_ut/online_dispatch/test_utils_online_dispatch.py
@@ -23,42 +23,71 @@ import logging
 from pathlib import Path
 from unittest.mock import patch, MagicMock
 
-from msprobe.pytorch.online_dispatch.utils import COLOR_RED, COLOR_CYAN, COLOR_YELLOW, COLOR_RESET, COMPARE_LOGO, data_to_cpu, DispatchException
+from msprobe.pytorch.online_dispatch.utils import COLOR_RED, COLOR_CYAN, COLOR_YELLOW, COLOR_RESET, COMPARE_LOGO, \
+    data_to_cpu, DispatchException
 
 cpu_device = torch._C.device("cpu")
 
+
 class FakeData:
     def init(self):
-        self.numpy=np.random.rand(5,5)
+        self.numpy = np.random.rand(5, 5)
+
 
 class FakeDataNoNumpy:
     def init(self):
-        self.data=np.random.rand(5,5)
+        self.data = np.random.rand(5, 5)
+
 
 class TestUtils(unittest.TestCase):
     def setUp(self):
-        self.stack=inspect.stack()
-        self.data_path=""
-        self.file_name="data"
-        self.data=FakeData()
-        self.data_nonumpy=FakeDataNoNumpy()
-        self.dispatch_exception=DispatchException(err_code=1, err_msg="messages")
+        self.stack = inspect.stack()
+        self.data_path = ""
+        self.file_name = "data"
+        self.data = FakeData()
+        self.data_nonumpy = FakeDataNoNumpy()
+        self.dispatch_exception = DispatchException(err_code=1, err_msg="messages")
         Path(os.path.join(self.data_path, f'{self.file_name}.npy')).touch()
+
     def tearDown(self):
         if os.path.exists(os.path.join(self.data_path, f'{self.file_name}.npy')):
             os.remove(os.path.join(self.data_path, f'{self.file_name}.npy'))
 
-    def test_data_to_cpu_should_return_tensor_copy_when_input_tensor(self):
-        data = torch.tensor([1,2],device=cpu_device,dtype=torch.float16)
-        deep=1
-        data_cpu=[]
-        self.assertEqual(data_to_cpu(data,deep,data_cpu).all(),data.clone().detach().float().all())
+    def test_get_callstack(self):
+        a = 1
 
-    def test_data_to_cpu_should_return_list_when_input_list(self):
-        data=[1,2]
-        deep=0
-        data_cpu=[]
-        self.assertEqual(data_to_cpu(data,deep,data_cpu), data)
+    def test_data_to_cpu_tensor(self):
+        data = torch.tensor([1, 2], device=cpu_device, dtype=torch.float16)
+        deep = 1
+        data_cpu = []
+        self.assertEqual(data_to_cpu(data, deep, data_cpu).all(), data.clone().detach().float().all())
+
+    def test_data_to_cpu_list(self):
+        data = [1, 2]
+        deep = 0
+        data_cpu = []
+        self.assertEqual(data_to_cpu(data, deep, data_cpu), data)
+
+    def test_data_to_cpu_tuple(self):
+        data = [1, 2]
+        deep = 0
+        data_cpu = []
+        self.assertEqual(data_to_cpu(data, deep, data_cpu), data)
+
+    def test_data_to_cpu_dict(self):
+        data = [1, 2]
+        deep = 0
+        data_cpu = []
+        self.assertEqual(data_to_cpu(data, deep, data_cpu), data)
+
+    def test_data_to_cpu_c_device(self):
+        data = [1, 2]
+        deep = 0
+        data_cpu = []
+        self.assertEqual(data_to_cpu(data, deep, data_cpu), data)
 
     def test_str(self):
         self.assertEqual(self.dispatch_exception.__str__(),"messages")
+
+    def test_get_sys_info(self):
+        a = 1
