b3307057495442a2f8ee61404c6ae0f64950823f
l30036321 <lvkaimeng@huawei.com>
2024-10-29 10:43:14 +0800

    fix index error issue

--- debug/accuracy_tools/msprobe/mindspore/compare/ms_compare.py
+++ debug/accuracy_tools/msprobe/mindspore/compare/ms_compare.py
@@ -201,6 +201,11 @@ class MSComparator(Comparator):
                     if idx not in pt_para_list:
                         self.remove_element(bench_op_name, bench_struct_in, bench_summary, idx)
                 npu_op_name = self.api_replace(npu_op_name, ms_api_name, pt_api_name)
+                if len(npu_op_name) != len(bench_op_name):
+                    logger.warning(
+                        "The total number of input and output parameters of \
+                            npu_op_name and bench_op_name are not equal.")
+                    break
                 npu_op_name = self.para_sequence_update(npu_op_name, bench_op_name)
                 target_dict = api_dict
                 break
