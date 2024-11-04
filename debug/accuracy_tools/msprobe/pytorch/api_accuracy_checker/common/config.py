7f5b1f3e2f45a660526c33e32f58932be612e231
gitee <jiangchangting@huawei.com>
2024-11-04 16:27:46 +0800

    fix

--- debug/accuracy_tools/msprobe/pytorch/api_accuracy_checker/common/config.py
+++ debug/accuracy_tools/msprobe/pytorch/api_accuracy_checker/common/config.py
@@ -51,7 +51,7 @@ class Config:
             raise ValueError(f"{key} must be one of {validators.keys()}")
         if not isinstance(value, validators.get(key)):
             raise ValueError(f"{key} must be {validators[key].__name__} type")
-        if key == 'precision' and is_int(value) and (value < 0 or value > 20):
+        if key == 'precision' and not is_int(value) and (value < 0 or value > 20):
             raise ValueError("precision must be greater than or equal to 0 and less than 21")
         if key == 'white_list':
             RunUTConfig.check_filter_list_config(key, value)
