aa1814b8961325c5efa61f6fb79bf19d33110633
wangqingcai <wqc01202410@gmail.com>
2024-12-05 18:34:40 +0800

    add monitor ut

--- debug/accuracy_tools/msprobe/test/pytorch_ut/monitor/test_monitor_wrap_distributed.py
+++ debug/accuracy_tools/msprobe/test/pytorch_ut/monitor/test_monitor_wrap_distributed.py
@@ -1,12 +1,15 @@
 import unittest
+from collections import defaultdict
 from unittest.mock import patch, MagicMock
 import torch
 from torch import distributed as dist
+from msprobe.pytorch.monitor.module_hook import CommunicationContext
 
-from msprobe.pytorch.monitor.distributed.wrap_distributed import get_distributed_ops, DistributedOPTemplate, ApiRegistry, \
-    get_process_group, stack_filter, get_callstack, op_aggregate, update_data, is_target_line, create_async_callback_func, \
-    ORIGIN_WAIT, PENDING_ASYNC_CC_BY_HANDLE
-
+from msprobe.pytorch.monitor.distributed.wrap_distributed import get_distributed_ops, DistributedOPTemplate, \
+    ApiRegistry, \
+    get_process_group, stack_filter, get_callstack, op_aggregate, update_data, is_target_line, \
+    create_async_callback_func, \
+    ORIGIN_WAIT, PENDING_ASYNC_CC_BY_HANDLE, catch_data, create_hooks
 
 WrapDistributedOps = ['all_reduce', 'broadcast', 'all_gather']
 
@@ -16,7 +19,7 @@ class TestGetDistributedOps(unittest.TestCase):
     def test_get_distributed_ops(self):
         expected = {'all_reduce', 'broadcast', 'all_gather'}
         with patch('msprobe.pytorch.monitor.distributed.wrap_distributed.WrapDistributedOps', \
-            new=['all_reduce', 'broadcast', 'all_gather']): 
+                   new=['all_reduce', 'broadcast', 'all_gather']):
             result = get_distributed_ops()
 
         self.assertEqual(result, expected)
@@ -24,9 +27,9 @@ class TestGetDistributedOps(unittest.TestCase):
     def test_get_distributed_ops_with_non_existent_op(self):
         expected = {'all_reduce', 'broadcast'}
         with patch('msprobe.pytorch.monitor.distributed.wrap_distributed.WrapDistributedOps', \
-            new=['all_reduce', 'broadcast', 'non_existent_op']):
+                   new=['all_reduce', 'broadcast', 'non_existent_op']):
             result = get_distributed_ops()
-        
+
         self.assertEqual(result, expected)
 
     def test_get_distributed_ops_only_exclusions(self):
@@ -38,7 +41,7 @@ class TestGetDistributedOps(unittest.TestCase):
 
 
 class TestDistributedOPTemplate(unittest.TestCase):
-    
+
     def hook(name):
         def forward_pre_hook(nope, input, kwargs):
             pass
@@ -62,7 +65,7 @@ class TestApiRegistry(unittest.TestCase):
         self.ApiRegistry = ApiRegistry()
         global ORIGIN_WAIT
         global PENDING_ASYNC_CC_BY_HANDLE
-        self.attr_dict = {"b2":2, "b3":3}
+        self.attr_dict = {"b2": 2, "b3": 3}
 
     def hook(name):
         def forward_pre_hook(nope, input, kwargs):
@@ -72,21 +75,22 @@ class TestApiRegistry(unittest.TestCase):
             pass
 
         return forward_pre_hook, forward_hook
-        
+
     def tearDown(self) -> None:
         # 清空 PENDING_ASYNC_CC_BY_HANDLE
         PENDING_ASYNC_CC_BY_HANDLE.clear()
-    
+
     def test_store_ori_attr(self):
         class A():
             a1 = 1
+
         class B():
             a = A()
             b1 = 1
             b2 = 2
-        
+
         api_list = ["a.a1", "b1", "b2"]
-        expect_output = {"a.a1":1, "b1":1, "b2":2}
+        expect_output = {"a.a1": 1, "b1": 1, "b2": 2}
         actual_output = dict()
         ApiRegistry.store_ori_attr(B, api_list, actual_output)
         self.assertEqual(actual_output, expect_output)
@@ -94,18 +98,19 @@ class TestApiRegistry(unittest.TestCase):
     def test_set_api_attr(self):
         class A():
             a1 = 1
+
         class B():
             a = A().__class__
             b1 = 1
-        
-        attr_dict = {"a.a2":2, "b2":2, "b3":3}
+
+        attr_dict = {"a.a2": 2, "b2": 2, "b3": 3}
         ApiRegistry.set_api_attr(B, attr_dict)
 
         for k, v in attr_dict.items():
             if '.' in k:
                 sub_module_name, sub_op = k.rsplit('.', 1)
                 sub_module = getattr(B, sub_module_name, None)
-                
+
                 self.assertEqual(getattr(sub_module, sub_op), v)
             else:
                 self.assertEqual(getattr(B, k), v)
@@ -117,7 +122,7 @@ class TestApiRegistry(unittest.TestCase):
         # 注册一个待处理的函数
         mock_wait.return_value = MagicMock()
         mock_handle["handle"] = MagicMock()
-        
+
         # 执行 redirect_wait
         ApiRegistry.redirect_wait()
 
@@ -127,7 +132,7 @@ class TestApiRegistry(unittest.TestCase):
 
         # 验证 ORIGIN_WAIT 被调用
         mock_wait.assert_called_once_with("handle")
-        
+
     def test_redirect_api(self):
         self.ApiRegistry.distributed_attr_hooked = self.attr_dict
         self.ApiRegistry.redirect_api()
@@ -144,7 +149,7 @@ class TestApiRegistry(unittest.TestCase):
 
     def test_initialize_hook(self):
         self.ApiRegistry.initialize_hook([self.hook()[0]], [self.hook()[1]])
-        
+
         self.assertEqual(len(get_distributed_ops()), len(self.ApiRegistry.distributed_attr_origin))
         self.assertEqual(len(get_distributed_ops()), len(self.ApiRegistry.distributed_attr_hooked))
 
@@ -220,12 +225,17 @@ class TestFunctions(unittest.TestCase):
 
         self.assertEqual(op_aggregate('mean', tensorlist), 2)
 
+    def test_op_aggregate_with_default_op(self):
+        tensorlist = [1, 2, 3]
+        res = op_aggregate('test_op', tensorlist)
+        self.assertTrue(res.isnan().item())
+
     def test_op_aggregate_other(self):
         self.assertTrue(torch.isnan(op_aggregate('', None)))
 
     def test_update_data_new(self):
         old = {}
-        new = {'tag1':{'op1': torch.tensor([1, 2, 3])}}
+        new = {'tag1': {'op1': torch.tensor([1, 2, 3])}}
         expected = torch.tensor([1, 2, 3])
         old = update_data(old, new)
 
@@ -233,8 +243,8 @@ class TestFunctions(unittest.TestCase):
         self.assertTrue(torch.equal(old['tag1']['op1'][0], expected))
 
     def test_update_data_append(self):
-        old = {'tag1':{'op1': [torch.tensor([1, 2, 3])]}}
-        new = {'tag1':{'op1': torch.tensor([2, 3, 4]), 'op2': torch.tensor([3, 4, 5])}}
+        old = {'tag1': {'op1': [torch.tensor([1, 2, 3])]}}
+        new = {'tag1': {'op1': torch.tensor([2, 3, 4]), 'op2': torch.tensor([3, 4, 5])}}
         old = update_data(old, new)
 
         self.assertIsInstance(old['tag1']['op1'], list)
@@ -273,6 +283,92 @@ class TestFunctions(unittest.TestCase):
 
         # 验证 catch_data 是否被调用，并且传递的参数正确
         mock_catch_data.assert_called_once_with(context, cc_name, ops, args, prefix)
+
+
+class TestCatchData(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.cc_context = CommunicationContext()
+        self.cc_name = 'cc_name'
+        self.ops = ["min", "max"]
+        self.prefix = 'prefix'
+        self.target_key = "cc_name/prefix_0"
+
+    def test_catch_data_with_tensor(self):
+        args = [torch.tensor([1, 2, 3])]
+        catch_data(self.cc_context, self.cc_name, self.ops, args, self.prefix)
+        self.assertIn(self.target_key, self.cc_context.data)
+        for op in self.ops:
+            self.assertIn(op, self.cc_context.data[self.target_key])
+
+    def test_catch_data_with_list_of_tensors(self):
+        args = [[torch.tensor([1, 2, 3]), torch.tensor([4, 5, 6])]]
+        catch_data(self.cc_context, self.cc_name, self.ops, args, self.prefix)
+        self.assertIn(self.target_key, self.cc_context.data)
+        for op in self.ops:
+            self.assertIn(op, self.cc_context.data[self.target_key])
+
+
+class TestCreateHooks(unittest.TestCase):
+
+    def setUp(self) -> None:
+        class MockMonitor:
+            cc_logged_stack = defaultdict(set)
+            cc_codeline = []
+            ops = ["min", "max"]
+            module_rank_list = []
+            cc_log_only = False
+            cc_pre_hook = False
+            cc_context = defaultdict(CommunicationContext)
+
+        self.monitor = MockMonitor()
+        self.context = self.monitor.cc_context
+        self.dist_mock = MagicMock()
+        self.dist_mock.get_rank.return_value = 0
+        self.dist_mock.is_initialized.return_value = True
+        dist.is_initialized = self.dist_mock.is_initialized
+        dist.get_rank = self.dist_mock.get_rank
+
+    def test_create_hooks_without_hook(self):
+        self.monitor.module_rank_list = [0]
+        pre_hooks, hooks = create_hooks(self.context, self.monitor)
+        self.assertEqual(len(pre_hooks), 0)
+        self.assertEqual(len(hooks), 1)
+
+    def test_create_hooks_with_cc_log_only(self):
+        self.monitor.cc_log_only = True
+        pre_hooks, hooks = create_hooks(self.context, self.monitor)
+        self.assertEqual(hooks, [])
+        cc_log_hook = pre_hooks[0]
+
+        mock_get_callstack = MagicMock()
+        mock_get_callstack.return_value = "test string"
+        mock_module = MagicMock()
+        mock_module.op_name_ = "op"
+        cc_log_hook(mock_module, None, None)
+        self.assertIn("op", self.monitor.cc_logged_stack)
+        self.assertEqual(1, len(self.monitor.cc_logged_stack["op"]))
+
+    def test_create_hooks_with_cc_pre_hook_and_cc_hook(self):
+        self.monitor.cc_pre_hook = True
+        pre_hooks, hooks = create_hooks(self.context, self.monitor)
+        self.assertEqual(1, len(pre_hooks))
+        self.assertEqual(1, len(hooks))
+
+        cc_pre_hook = pre_hooks[0]
+        mock_module = MagicMock()
+        mock_module.op_name_ = "test_module"
+        args = tuple([torch.tensor([1, 2, 3])])
+        kwargs = {}
+        cc_pre_hook(mock_module, args, kwargs)
+        self.assertIn("test_module", self.context)
+        self.assertIsInstance(self.context["test_module"], CommunicationContext)
+
+        cc_hook = hooks[0]
+        cc_hook(mock_module, args, kwargs)
+
+        res = cc_hook(mock_module, args, kwargs, [])
+        self.assertEqual(res, [])
 
 
 if __name__ == '__main__':
