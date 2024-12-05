f6063c253ea30fa78904d1c595f6a72b8999ff9b
wangchao426 <wangchaottss@sina.com>
2024-12-05 21:23:57 +0800

    【bugfix】添加比对结果默认目录，默认目录为./output/

--- debug/accuracy_tools/msprobe/test/core_ut/compare/test_utils.py
+++ debug/accuracy_tools/msprobe/test/core_ut/compare/test_utils.py
@@ -8,6 +8,6 @@ class TestUtils(unittest.TestCase):
     def test_compare_parser(self):
         parser = argparse.ArgumentParser()
         _compare_parser(parser)
-        self.assertEqual(parser.parse_args('-i aaa -o'.split(' ')), './output')
-        self.assertEqual(parser.parse_args('-i aaa'.split(' ')), './output')
-        self.assertEqual(parser.parse_args('-i aaa -o ./aaa/output'.split(' ')), './aaa/output')
+        self.assertEqual(parser.parse_args('-i aaa -o'.split(' ')).output_path, './output')
+        self.assertEqual(parser.parse_args('-i aaa'.split(' ')).output_path, './output')
+        self.assertEqual(parser.parse_args('-i aaa -o ./aaa/output'.split(' ')).output_path, './aaa/output')
