d904cb9a9d7fa2e6f83cf92206715c3a5b8011e0
fanglanyue <fanglanyue@huawei.com>
2025-02-05 10:53:23 +0800

    add copyright for profielr/msprof_analyze/test

--- profiler/msprof_analyze/test/st/cluster_analyse/cluster_communication_analyzer_time_db.py
+++ profiler/msprof_analyze/test/st/cluster_analyse/cluster_communication_analyzer_time_db.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2025, Huawei Technologies Co., Ltd.
+# All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0  (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 class ClusterCommunicationAnalyzerTime:
     def __init__(self, rank_set=None, step=None, rank_id=None, hccl_op_name=None, group_name=None,
                  start_timestamp=None, elapsed_time=None, transit_time=None, wait_time=None,
