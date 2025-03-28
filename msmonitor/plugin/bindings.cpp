77b799f76fbfe2a69cc69747f9a8dadd53171366
Gallium <hujia23@huawei.com>
2025-03-28 14:46:20 +0800

    dynolog 安全编译选项

--- msmonitor/plugin/bindings.cpp
+++ msmonitor/plugin/bindings.cpp
@@ -1,14 +1,31 @@
+
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include "ipc_monitor/PyDynamicMonitorProxy.h"
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(IPCMonitor, m) {
-   py::class_<dynolog_npu::ipc_monitor::PyDynamicMonitorProxy>(m, "PyDynamicMonitorProxy")
+void init_IPCMonitor(PyObject *module) {
+    py::class_<dynolog_npu::ipc_monitor::PyDynamicMonitorProxy>(module, "PyDynamicMonitorProxy")
         .def(py::init<>())
         .def("init_dyno", &dynolog_npu::ipc_monitor::PyDynamicMonitorProxy::InitDyno, py::arg("npuId"))
         .def("poll_dyno", &dynolog_npu::ipc_monitor::PyDynamicMonitorProxy::PollDyno)
         .def("enable_dyno_npu_monitor", &dynolog_npu::ipc_monitor::PyDynamicMonitorProxy::EnableMsptiMonitor, py::arg("cfg_map"))
         .def("finalize_dyno", &dynolog_npu::ipc_monitor::PyDynamicMonitorProxy::FinalizeDyno);
+}
+
+static PyMethodDef g_moduleMethods[] = {};
+
+static struct PyModuleDef ipcMonitor_module = {
+    PyModuleDef_HEAD_INIT,
+    "IPCMonitor",
+    nullptr,
+    -1,
+    g_moduleMethods
+};
+
+PyMODINIT_FUNC PyInit_IPCMonitor(void) {
+    PyObject* m = PyModule_Create(&ipcMonitor_module);
+    init_IPCMonitor(m);
+    return m;
 }
