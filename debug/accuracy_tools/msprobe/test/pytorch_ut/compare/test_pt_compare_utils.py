e11894282fc719660a0b3e42b35c95fc0abbfe20
Linwei-Ying <lwying007@126.com>
2025-04-03 14:13:35 +0800

    compare_reconstruct3

--- debug/accuracy_tools/msprobe/test/pytorch_ut/compare/test_pt_compare_utils.py
+++ debug/accuracy_tools/msprobe/test/pytorch_ut/compare/test_pt_compare_utils.py
@@ -1,43 +1,34 @@
+import os
+import shutil
+import threading
 import unittest
-from unittest.mock import patch, MagicMock
 
-import torch
 import numpy as np
 
-from msprobe.core.common.utils import CompareException
-from msprobe.core.common.file_utils import FileCheckConst
 from msprobe.pytorch.compare.utils import read_pt_data
+from msprobe.test.core_ut.compare.test_acc_compare import generate_pt
+
+
+base_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), f'test_pt_compare_utils_data')
+pt_dir = os.path.join(base_dir, f'dump_data_dir')
 
 
 class TestReadPtData(unittest.TestCase):
 
-    @patch('msprobe.pytorch.compare.utils.load_pt')
-    @patch('msprobe.pytorch.compare.utils.FileChecker')
-    @patch('os.path.join', return_value='/fake/path/to/file.pt')
-    def test_read_pt_data(self, mock_os, mock_file_checker, mock_load_pt):
-        mock_file_checker.return_value.common_check.return_value = '/fake/path/to/file.pt'
+    def setUp(self):
+        os.makedirs(base_dir, mode=0o750, exist_ok=True)
+        os.makedirs(pt_dir, mode=0o750, exist_ok=True)
 
-        mock_tensor = MagicMock()
-        mock_tensor.detach.return_value = mock_tensor
-        mock_tensor.to.return_value = mock_tensor
-        mock_tensor.dtype = torch.bfloat16
-        mock_tensor.numpy.return_value = np.array([1.0, 2.0, 3.0])
-        mock_load_pt.return_value = mock_tensor
+        self.lock = threading.Lock()
 
-        result = read_pt_data('/fake/dir', 'file_name.pt')
+    def tearDown(self):
+        if os.path.exists(pt_dir):
+            shutil.rmtree(pt_dir)
+        if os.path.exists(base_dir):
+            shutil.rmtree(base_dir)
 
-        mock_file_checker.assert_called_once_with('/fake/path/to/file.pt', FileCheckConst.FILE, FileCheckConst.READ_ABLE, FileCheckConst.PT_SUFFIX, False)
-        mock_load_pt.assert_called_once_with('/fake/path/to/file.pt', to_cpu=True)
-        mock_tensor.to.assert_called_once_with(torch.float32)
-        self.assertTrue(np.array_equal(result, np.array([1.0, 2.0, 3.0])))
-
-    @patch('os.path.join', return_value='/fake/path/to/file.pt')
-    @patch('msprobe.pytorch.compare.utils.FileChecker')
-    @patch('msprobe.pytorch.compare.utils.load_pt')
-    def test_read_real_data_pt_exception(self, mock_load_pt, mock_file_checker, mock_os):
-        mock_file_checker.return_value.common_check.return_value = '/fake/path/to/file.pt'
-
-        mock_load_pt.side_effect = RuntimeError("Test Error")
-
-        with self.assertRaises(CompareException):
-            read_pt_data('/fake/dir', 'file_name.pt')
+    def test_read_pt_data(self):
+        generate_pt(pt_dir)
+        result = read_pt_data(pt_dir, 'Functional.linear.0.forward.input.0.pt')
+        expected = np.array([1.0, 2.0, 3.0, 4.0])
+        self.assertTrue(np.array_equal(result, expected))
