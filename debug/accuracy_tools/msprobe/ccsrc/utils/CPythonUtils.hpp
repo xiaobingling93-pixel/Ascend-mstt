ca3660516e6846350eb04d17914505a1e1321882
gitee <jiangchangting@huawei.com>
2025-04-29 15:53:02 +0800

    cleancode

--- debug/accuracy_tools/msprobe/ccsrc/utils/CPythonUtils.hpp
+++ debug/accuracy_tools/msprobe/ccsrc/utils/CPythonUtils.hpp
@@ -85,7 +85,7 @@ public:
         Py_INCREF(Py_None);
         ptr = Py_None;
     }
-    explicit PythonObject(PyObject* o) : ptr(o) {
+    PythonObject(PyObject* o) : ptr(o) {
         if (ptr == nullptr) {
             ptr = Py_None;
         }
@@ -185,7 +185,7 @@ private:
 class PythonNumberObject : public PythonObject {
 public:
     PythonNumberObject();
-    explicit PythonNumberObject(PyObject* o);
+    PythonNumberObject(PyObject* o);
 
     static PythonNumberObject From(const int32_t& input);
     static PythonNumberObject From(const uint32_t& input);
@@ -195,7 +195,7 @@ public:
 class PythonStringObject : public PythonObject {
 public:
     PythonStringObject();
-    explicit PythonStringObject(PyObject* o);
+    PythonStringObject(PyObject* o);
 
     static PythonStringObject From(const std::string& input);
     static PythonStringObject From(const char* input);
@@ -204,7 +204,7 @@ public:
 class PythonBoolObject : public PythonObject {
 public:
     PythonBoolObject();
-    explicit PythonBoolObject(PyObject* o);
+    PythonBoolObject(PyObject* o);
 
     static PythonBoolObject From(const bool& input);
 };
@@ -213,7 +213,7 @@ class PythonListObject : public PythonObject {
 public:
     PythonListObject();
     explicit PythonListObject(size_t size);
-    explicit PythonListObject(PyObject* o);
+    PythonListObject(PyObject* o);
 
     template <typename T>
     static PythonListObject From(const std::vector<T>& input);
@@ -230,7 +230,7 @@ public:
 class PythonTupleObject : public PythonObject {
 public:
     PythonTupleObject();
-    explicit PythonTupleObject(PyObject* o);
+    PythonTupleObject(PyObject* o);
 
     template <typename T>
     static PythonTupleObject From(const std::vector<T>& input);
@@ -242,7 +242,7 @@ public:
 class PythonDictObject : public PythonObject {
 public:
     PythonDictObject();
-    explicit PythonDictObject(PyObject* o);
+    PythonDictObject(PyObject* o);
 
     template <typename T1, typename T2>
     static PythonDictObject From(const std::map<T1, T2>& input);
