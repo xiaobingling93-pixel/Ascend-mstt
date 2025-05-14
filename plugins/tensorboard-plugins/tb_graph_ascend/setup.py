d7e98e56d165d4d543ea5df0aa0cf6a14c539065
sunchao <1299792067@qq.com>
2025-05-14 15:41:37 +0800

    🦄 refactor: 代码分割

--- plugins/tensorboard-plugins/tb_graph_ascend/setup.py
+++ plugins/tensorboard-plugins/tb_graph_ascend/setup.py
@@ -16,7 +16,7 @@
 # --------------------------------------------------------------------------------------------#
 import setuptools
 
-VERSION = '1.0.3'
+VERSION = '2.0.0'
 INSTALL_REQUIRED = ["tensorboard >= 2.11.2"]
 
 setuptools.setup(
