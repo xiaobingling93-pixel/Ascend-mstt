32ac2403a6542dddfb41ed2393efbda1677ae439
wangchao <15045058849@163.com>
2025-05-27 20:46:28 +0800

    add core base precison debugger

---
+++ debug/accuracy_tools/msprobe/core/debugger/precision_debugger.py
@@ -0,0 +1,89 @@
+# Copyright (c) 2025-2025, Huawei Technologies Co., Ltd.
+# All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0  (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import os
+
+from msprobe.core.common.const import Const, FileCheckConst
+from msprobe.core.common.exceptions import MsprobeException
+from msprobe.core.common.file_utils import FileChecker, load_json
+from msprobe.core.common.utils import get_real_step_or_rank
+from msprobe.core.common_config import CommonConfig
+
+
+class BasePrecisionDebugger:
+    _instance = None
+    tasks_not_need_debugger = [Const.GRAD_PROBE]
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(BasePrecisionDebugger, cls).__new__(cls)
+            cls._instance.config = None
+            cls._instance.enable_dataloader = False
+        return cls._instance
+
+    def __init__(
+            self,
+            config_path=None,
+            task=None,
+            dump_path=None,
+            level=None,
+            step=None
+    ):
+        self.check_input_params(config_path, task, dump_path, level)
+        self.common_config, self.task_config = self.parse_config_path(config_path, task)
+        self.task = self.common_config.task
+        if step is not None:
+            self.common_config.step = get_real_step_or_rank(step, Const.STEP)
+
+    @staticmethod
+    def check_input_params(config_path, task, dump_path, level):
+        if not config_path:
+            config_path = os.path.join(os.path.dirname(__file__), "../../config.json")
+        if config_path is not None:
+            if not isinstance(config_path, str):
+                raise MsprobeException(
+                    MsprobeException.INVALID_PARAM_ERROR, f"config_path must be a string")
+            file_checker = FileChecker(
+                file_path=config_path, path_type=FileCheckConst.FILE, file_type=FileCheckConst.JSON_SUFFIX)
+            file_checker.common_check()
+
+        if task is not None and task not in Const.TASK_LIST:
+            raise MsprobeException(
+                MsprobeException.INVALID_PARAM_ERROR, f"task must be one of {Const.TASK_LIST}")
+
+        if dump_path is not None:
+            if not isinstance(dump_path, str):
+                raise MsprobeException(
+                    MsprobeException.INVALID_PARAM_ERROR, f"dump_path must be a string")
+
+        if level is not None and level not in Const.LEVEL_LIST:
+            raise MsprobeException(
+                MsprobeException.INVALID_PARAM_ERROR, f"level must be one of {Const.LEVEL_LIST}")
+
+    def parse_config_path(self, json_file_path, task):
+        if not json_file_path:
+            raise Exception("json file path is None")
+        json_config = load_json(json_file_path)
+        common_config = CommonConfig(json_config)
+        if task:
+            task_config = self.get_task_config(task, json_config)
+        else:
+            if not common_config.task:
+                common_config.task = Const.STATISTICS
+            task_config = self.get_task_config(common_config.task, json_config)
+        return common_config, task_config
+
+    def get_task_config(self):
+        raise NotImplementedError("Subclass must implment get_task_config")
+
