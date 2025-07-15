c3f694d3b95376d9ef7d0ba882b4f53b1c913031
sunchao <1299792067@qq.com>
2025-07-15 20:30:24 +0800

    🐞 fix: 去除print

--- plugins/tensorboard-plugins/tb_graph_ascend/test/integration/views/test_graph_views.py
+++ plugins/tensorboard-plugins/tb_graph_ascend/test/integration/views/test_graph_views.py
@@ -85,7 +85,6 @@ class TestGraphViews:
         excepted = test_case['excepted']
         # 获取响应内容
         response_body = json.loads(b''.join(response_iter).decode('utf-8'))
-        print(response_body)
         assert response_body == excepted
         assert TestGraphViews.captured.status == "200 OK"
         assert TestGraphViews.captured.headers["Content-Type"] == "application/json"
