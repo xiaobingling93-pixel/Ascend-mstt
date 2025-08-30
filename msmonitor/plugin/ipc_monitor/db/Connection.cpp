1910b9dcb3b51c5d9731a1bb166e4d582684545a
wjchuee <wjchuee@foxmail.com>
2025-08-30 14:50:40 +0800

    msmonitor db develop part 2

---
+++ msmonitor/plugin/ipc_monitor/db/Connection.cpp
@@ -0,0 +1,228 @@
+/*
+ * Copyright (C) 2025-2025. Huawei Technologies Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "db/Connection.h"
+#include "utils.h"
+
+namespace {
+constexpr int32_t TIMEOUT = INT32_MAX;
+const std::string CREATE_TABLE = "CREATE TABLE";
+const std::string CREATE_INDEX = "CREATE INDEX";
+const std::string DROP_TABLE = "DROP TABLE";
+const std::string UPDATE = "UPDATE";
+const std::string DELETE = "DELETE";
+const std::string CHECK = "CHECK";
+}
+
+namespace dynolog_npu {
+namespace ipc_monitor {
+namespace db {
+Connection::Connection(const std::string &path)
+{
+    auto rc = sqlite3_open(path.c_str(), &db_);
+    if (rc != SQLITE_OK) {
+        LOG(ERROR) << "Open database failed: " << rc << ", msg: " << sqlite3_errmsg(db_);
+        sqlite3_close_v2(db_);
+        db_ = nullptr;
+    } else {
+        sqlite3_exec(db_, "PRAGMA synchronous=OFF;", nullptr, nullptr, nullptr);
+    }
+}
+
+Connection::~Connection()
+{
+    if (stmt_) {
+        sqlite3_finalize(stmt_);
+    }
+    if (db_) {
+        auto rc = sqlite3_close(db_);
+        if (rc != SQLITE_OK) {
+            LOG(ERROR) << "Close database failed: " << rc << ", msg: " << sqlite3_errmsg(db_);
+            sqlite3_close_v2(db_);
+        }
+        db_ = nullptr;
+    }
+}
+
+bool Connection::ExecuteSql(const std::string &sql, const std::string &sqlType)
+{
+    CHAR_PTR errMsg{nullptr};
+    sqlite3_busy_timeout(db_, TIMEOUT);
+    auto rc = sqlite3_exec(db_, sql.c_str(), nullptr, nullptr, &errMsg);
+    if (rc != SQLITE_OK) {
+        if (sqlType == CHECK) {
+            LOG(WARNING) << "Execute sql failed: " << rc << ", type: " << sqlType << ", msg: " << errMsg;
+        } else {
+            LOG(ERROR) << "Execute sql failed: " << rc << ", type: " << sqlType << ", msg: " << errMsg;
+        }
+        sqlite3_free(errMsg);
+        return false;
+    }
+    return true;
+}
+
+bool Connection::CheckTableExists(const std::string &tableName)
+{
+    std::string sql = "SELECT COUNT(1) FROM " + tableName;
+    return ExecuteSql(sql, CHECK);
+}
+
+bool Connection::ExecuteCreateTable(const std::string &sql)
+{
+    return ExecuteSql(sql, CREATE_TABLE);
+}
+
+bool Connection::ExecuteCreateIndex(const std::string &sql)
+{
+    return ExecuteSql(sql, CREATE_INDEX);
+}
+
+bool Connection::ExecuteDropTable(const std::string &sql)
+{
+    return ExecuteSql(sql, DROP_TABLE);
+}
+
+bool Connection::ExecuteUpdate(const std::string &sql)
+{
+    return ExecuteSql(sql, UPDATE);
+}
+
+bool Connection::ExecuteDelete(const std::string &sql)
+{
+    return ExecuteSql(sql, DELETE);
+}
+
+std::vector<TableColumn> Connection::ExecuteGetTableColumns(const std::string &tableName)
+{
+    std::vector<TableColumn> columns;
+    std::string sql = "PRAGMA table_info(" + tableName + ")";
+    sqlite3_busy_timeout(db_, TIMEOUT);
+    auto rc = sqlite3_prepare_v2(db_, sql.c_str(), -1, &stmt_, nullptr);
+    if (rc != SQLITE_OK) {
+        LOG(ERROR) << "Execute sql failed: " << rc << ", msg: " << sqlite3_errmsg(db_);
+        return columns;
+    }
+    while (sqlite3_step(stmt_) == SQLITE_ROW) {
+        std::string name, type;
+        GetColumn(name);
+        GetColumn(type);
+        columns.emplace_back(name, type);
+        index_ = 0;
+    }
+    return columns;
+}
+
+bool Connection::InsertCmd(const std::string &tableName, uint32_t colNum)
+{
+    std::string sql = "INSERT INTO " + tableName + " VALUES (";
+    for (uint32_t i = 0; i < colNum; ++i) {
+        sql += "?";
+        if (i < colNum - 1) {
+            sql += ", ";
+        }
+    }
+    sql += ")";
+    sqlite3_busy_timeout(db_, TIMEOUT);
+    auto rc = sqlite3_prepare_v2(db_, sql.c_str(), -1, &stmt_, nullptr);
+    if (rc != SQLITE_OK) {
+        LOG(ERROR) << "Execute sql failed: " << rc << ", msg: " << sqlite3_errmsg(db_);
+        return false;
+    }
+    return true;
+}
+
+bool Connection::QueryCmd(const std::string &sql)
+{
+    auto rc = sqlite3_prepare_v2(db_, sql.c_str(), -1, &stmt_, nullptr);
+    if (rc != SQLITE_OK) {
+        LOG(ERROR) << "Execute sql failed: " << rc << ", msg: " << sqlite3_errmsg(db_);
+        return false;
+    }
+    return true;
+}
+
+bool Connection::BindParameters(int32_t value)
+{
+    return sqlite3_bind_int(stmt_, ++index_, value) == SQLITE_OK;
+}
+
+bool Connection::BindParameters(uint32_t value)
+{
+    return sqlite3_bind_int64(stmt_, ++index_, value) == SQLITE_OK;
+}
+
+bool Connection::BindParameters(int64_t value)
+{
+    return sqlite3_bind_int64(stmt_, ++index_, value) == SQLITE_OK;
+}
+
+bool Connection::BindParameters(uint64_t value)
+{
+    return sqlite3_bind_int64(stmt_, ++index_, value) == SQLITE_OK;
+}
+
+bool Connection::BindParameters(double value)
+{
+    return sqlite3_bind_double(stmt_, ++index_, value) == SQLITE_OK;
+}
+
+bool Connection::BindParameters(std::string value)
+{
+    return sqlite3_bind_text(stmt_, ++index_, value.c_str(), -1, SQLITE_TRANSIENT) == SQLITE_OK;
+}
+
+void Connection::GetColumn(uint16_t &value)
+{
+    value = static_cast<uint16_t>(sqlite3_column_int(stmt_, ++index_));
+}
+
+void Connection::GetColumn(int32_t &value)
+{
+    value = sqlite3_column_int(stmt_, ++index_);
+}
+
+void Connection::GetColumn(uint32_t &value)
+{
+    value = static_cast<uint32_t>(sqlite3_column_int64(stmt_, ++index_));
+}
+
+void Connection::GetColumn(int64_t &value)
+{
+    value = sqlite3_column_int64(stmt_, ++index_);
+}
+
+void Connection::GetColumn(uint64_t &value)
+{
+    value = static_cast<uint64_t>(sqlite3_column_int64(stmt_, ++index_));
+}
+
+void Connection::GetColumn(double &value)
+{
+    value = sqlite3_column_double(stmt_, ++index_);
+}
+
+void Connection::GetColumn(std::string &value)
+{
+    const unsigned char *text = sqlite3_column_text(stmt_, ++index_);
+    if (text == nullptr) {
+        value.clear();
+    } else {
+        value = std::string(ReinterpretConvert<const char*>(text));
+    }
+}
+} // namespace db
+} // namespace ipc_monitor
+} // namespace dynolog_npu
---
+++ msmonitor/plugin/ipc_monitor/db/DataBase.cpp
@@ -0,0 +1,149 @@
+/*
+ * Copyright (C) 2025-2025. Huawei Technologies Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "db/DataBase.h"
+#include "db/DBConstant.h"
+
+namespace dynolog_npu {
+namespace ipc_monitor {
+namespace db {
+namespace {
+const TableColumns STRING_IDS = {
+    {"id", SQL_INT_TYPE},
+    {"value", SQL_TEXT_TYPE}
+};
+
+const TableColumns ENUM_TABLE = {
+    {"id", SQL_INT_TYPE, true},
+    {"name", SQL_TEXT_TYPE}
+};
+
+const TableColumns META_DATA = {
+    {"name", SQL_TEXT_TYPE},
+    {"value", SQL_TEXT_TYPE}
+};
+
+const TableColumns HOST_INFO = {
+    {"hostUid", SQL_TEXT_TYPE},
+    {"hostName", SQL_TEXT_TYPE}
+};
+
+const TableColumns RANK_DEVICE_MAP = {
+    {"rankId", SQL_INT_TYPE},
+    {"deviceId", SQL_INT_TYPE}
+};
+
+const TableColumns CANN_API = {
+    {"startNs", SQL_INT_TYPE},
+    {"endNs", SQL_INT_TYPE},
+    {"type", SQL_INT_TYPE},
+    {"globalTid", SQL_INT_TYPE},
+    {"connectionId", SQL_INT_TYPE},
+    {"name", SQL_INT_TYPE}
+};
+
+const TableColumns TASK = {
+    {"startNs", SQL_INT_TYPE},
+    {"endNs", SQL_INT_TYPE},
+    {"deviceId", SQL_INT_TYPE},
+    {"connectionId", SQL_INT_TYPE},
+    {"globalTaskId", SQL_INT_TYPE},
+    {"globalPid", SQL_INT_TYPE},
+    {"taskType", SQL_INT_TYPE},
+    {"contextId", SQL_INT_TYPE},
+    {"streamId", SQL_INT_TYPE},
+    {"taskId", SQL_INT_TYPE},
+    {"modelId", SQL_INT_TYPE}
+};
+
+const TableColumns COMPUTE_TASK_INFO = {
+    {"name", SQL_INT_TYPE},
+    {"globalTaskId", SQL_INT_TYPE},
+    {"blockDim", SQL_INT_TYPE},
+    {"mixBlockDim", SQL_INT_TYPE},
+    {"taskType", SQL_INT_TYPE},
+    {"opType", SQL_INT_TYPE},
+    {"inputFormats", SQL_INT_TYPE},
+    {"inputDataTypes", SQL_INT_TYPE},
+    {"inputShapes", SQL_INT_TYPE},
+    {"outputFormats", SQL_INT_TYPE},
+    {"outputDataTypes", SQL_INT_TYPE},
+    {"outputShapes", SQL_INT_TYPE},
+    {"attrInfo", SQL_INT_TYPE},
+    {"opState", SQL_INT_TYPE},
+    {"hf32Eligible", SQL_INT_TYPE}
+};
+
+const TableColumns COMMUNICATION_OP = {
+    {"opName", SQL_INT_TYPE},
+    {"startNs", SQL_INT_TYPE},
+    {"endNs", SQL_INT_TYPE},
+    {"connectionId", SQL_INT_TYPE},
+    {"groupName", SQL_INT_TYPE},
+    {"opId", SQL_INT_TYPE},
+    {"relay", SQL_INT_TYPE},
+    {"retry", SQL_INT_TYPE},
+    {"dataType", SQL_INT_TYPE},
+    {"algType", SQL_INT_TYPE},
+    {"count", SQL_NUMERIC_TYPE},
+    {"opType", SQL_INT_TYPE}
+};
+
+const TableColumns MSTX = {
+    {"startNs", SQL_INT_TYPE},
+    {"endNs", SQL_INT_TYPE},
+    {"eventType", SQL_INT_TYPE},
+    {"rangeId", SQL_INT_TYPE},
+    {"category", SQL_INT_TYPE},
+    {"meassge", SQL_INT_TYPE},
+    {"globalTid", SQL_INT_TYPE},
+    {"endGlobalTid", SQL_INT_TYPE},
+    {"domainId", SQL_INT_TYPE},
+    {"connectionId", SQL_INT_TYPE}
+};
+} // namespace
+
+TableColumns Database::GetTableCols(const std::string &tableName)
+{
+    auto iter = tableColumns_.find(tableName);
+    if (iter == tableColumns_.end()) {
+        LOG(ERROR) << "Table " << tableName << " is not found";
+        return {};
+    }
+    return iter->second;
+}
+
+MsMonitorDB::MsMonitorDB()
+{
+    dbName_ = "msmonitor.db";
+    tableColumns_ = {
+        {TABLE_STRING_IDS, STRING_IDS},
+        {TABLE_COMMUNICATION_OP, COMMUNICATION_OP},
+        {TABLE_HCCL_DATA_TYPE, ENUM_TABLE},
+        {TABLE_MSTX, MSTX},
+        {TABLE_MSTX_EVENT_TYPE, ENUM_TABLE},
+        {TABLE_API_TYPE, ENUM_TABLE},
+        {TABLE_CANN_API, CANN_API},
+        {TABLE_TASK, TASK},
+        {TABLE_COMPUTE_TASK_INFO, COMPUTE_TASK_INFO},
+        {TABLE_META_DATA, META_DATA},
+        {TABLE_HOST_INFO, HOST_INFO},
+        {TABLE_RANK_DEVICE_MAP, RANK_DEVICE_MAP}
+    };
+}
+} // namespace db
+} // namespace ipc_monitor
+} // namespace dynolog_npu
